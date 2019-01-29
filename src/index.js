1355446a6cab8a5484a6326d1cfc305bb9f1531f
Marco Marasco <marco.marasco@pleaseup.com>
2019-01-29 10:50:24 +0100

    update serviceWorker references

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './bootstrap/App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './bootstrap/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
