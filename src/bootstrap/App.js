c81e8baad330ba6561f16fe99ff763176b1018fa
Marco Marasco <marco.marasco@pleaseup.com>
2019-01-29 10:44:41 +0100

    MetaMask handler wip

--- src/bootstrap/App.js
+++ src/bootstrap/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
-import logo from '../assets/logo.svg';
+import Blockies from 'react-blockies';
 import './App.css';
 
 // Web3 dapp utilities
 import { web3 } from './Dapp'
+var wallet = null;
+
+web3.eth.getAccounts((err, accounts) => {
+  console.log(accounts);
+  console.log(err);
+  wallet = accounts[0];
+})
+
+// provider change handler
+web3.currentProvider.connection.publicConfigStore.on('update', (ev) => console.log('MetaMask update', ev));
 
 class App extends Component {
   state = {
+    metamask: false,
     logged: false,
-    account: null
+    wallet: null
   }
 
   componentDidMount() {
-    web3.eth
-      .getAccounts()
-      .then((e) => {
-        console.log('web3 getAccounts()', e)
-        this.setState({
-          logged: true,
-          account: e.pop()
-        })
-      });
+
+    this.setState({ metamask: web3.currentProvider.host === 'metamask' })
+
+    web3.eth.getAccounts((err, wallets) => {
+      console.log(wallets);
+      console.log(err);
+      wallet = wallets[0];
+      this.setState({
+        logged: true,
+        wallet
+      })
+    })
   }
 
   render() {
@@ -30,7 +44,9 @@ class App extends Component {
           {
             this.state.logged
             ?
-              <p>Your current account is <code>{this.state.account}</code>.</p>
+              <div>
+                <p>Your current account is <Blockies seed={this.state.wallet} size={8} scale={6} bgColor="#486aad" color="#37cda9" spotColor="#96f490" /> <code>{this.state.wallet}</code></p>
+              </div>
             :
               <p>Please unlock <a href="https://metamask.io/">MetaMask</a></p>
           }
