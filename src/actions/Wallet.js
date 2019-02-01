56d0f4e89f669cd627f91cc0e044d58b36a0c395
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-02 00:24:55 +0100

    update wallet reducer and actions

--- src/actions/Wallet.js
+++ src/actions/Wallet.js
@@ -3,7 +3,17 @@ import { log } from "../utils/helpers"; // log helpers
 // Web3 dapp utilities
 import { web3 } from "../bootstrap/Dapp";
 
-import { SET_WALLET_ADDRESS } from "../reducers/types";
+import { SET_WALLET_ADDRESS, SET_WALLET_CONNECTION } from "../reducers/types";
+
+// Setters
+export const setWalletAddress = address => ({
+  type: SET_WALLET_ADDRESS,
+  payload: address
+});
+export const setWalletConnection = status => ({
+  type: SET_WALLET_CONNECTION,
+  payload: status
+});
 
 // Accounts
 export const fetchAccounts = () => {
--- src/reducers/Wallet.js
+++ src/reducers/Wallet.js
@@ -1,18 +1,23 @@
 import {
   SET_WALLET_ADDRESS,
+  SET_WALLET_CONNECTION,
   RESET_WALLET
-} from './types';
+} from "./types";
 
 const INITIAL_STATE = {
-  address: null
+  address: null,
+  isConnected: false,
+  user: {}
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-
     // Setters
     case SET_WALLET_ADDRESS:
       return { ...state, address: action.payload };
+
+    case SET_WALLET_CONNECTION:
+      return { ...state, isConnected: action.payload };
 
     // Reset
     case RESET_WALLET:
