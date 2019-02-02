30706ef4b2d4daa3714d56867bb2f760a259fdd7
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-02 13:14:25 +0100

    update wallet actions

--- src/actions/Wallet.js
+++ src/actions/Wallet.js
@@ -3,7 +3,11 @@ import { log } from "../utils/helpers"; // log helpers
 // Web3 dapp utilities
 import { web3 } from "../bootstrap/Dapp";
 
-import { SET_WALLET_ADDRESS, SET_WALLET_CONNECTION } from "../reducers/types";
+import {
+  SET_WALLET_ADDRESS,
+  SET_WALLET_CONNECTION,
+  RESET_WALLET
+} from "../reducers/types";
 
 // Setters
 export const setWalletAddress = address => ({
@@ -15,11 +19,16 @@ export const setWalletConnection = status => ({
   payload: status
 });
 
+// Reset
+export const resetWallet = () => ({
+  type: RESET_WALLET
+});
+
 // Accounts
 export const fetchAccounts = () => {
   return dispatch => {
     web3.eth.getAccounts((err, accounts) => {
-      log('fetchAccounts', { accounts, err }); // logs
+      log("fetchAccounts", { accounts, err }); // logs
 
       dispatch({ type: SET_WALLET_ADDRESS, payload: accounts[0] });
     });
