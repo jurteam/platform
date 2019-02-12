506ebb7b93e2e1e1d0cadcd8f591199edf5e1303
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-12 12:31:43 +0100

    update Unlock component refactoring

---
+++ src/components/auth/Unlock/Unlock.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import MetaMask from "../../../hooks/MetaMask"; // MetaMask hook
+import Button from "../../../components/common/Button";
+
+export class Unlock extends Component {
+  constructor(props) {
+    super(props);
+
+    this.unlock = this.unlock.bind(this);
+  }
+
+  unlock() {
+    MetaMask.auth();
+  }
+
+  render() {
+    // destructuring props
+    const { history } = this.props;
+
+    return (
+      <div className="jur--metamask--required">
+        <h2>MetaMask is Required</h2>
+        <hr />
+        <p>
+          MetaMask is an extension for accessing Ethereum enabled distributed
+          applications, or "Dapps" in your normal browser!
+          <br />
+          The extension injects the Ethereum web3 API into every website's
+          javascript context, so that dapps can read from the blockchain.
+        </p>
+        <Button
+          onClick={() => {
+            this.unlock();
+          }}
+          style={{ marginLeft: "10px" }}
+          name="Unlock MetaMask"
+        />
+        <Button
+          onClick={() => {
+            window.location.href = "https://metamask.io";
+          }}
+          style={{ marginLeft: "10px" }}
+          name="Get Chrome Extension"
+        />
+      </div>
+    );
+  }
+}
---
+++ src/components/auth/Unlock/index.js
@@ -0,0 +1,4 @@
+import { withRouter } from "react-router-dom";
+import { Unlock } from "./Unlock";
+
+export default withRouter(Unlock);
