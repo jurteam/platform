9f918f52b05d0fcf8fac5b96a484621ab4f0cefb
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-13 02:59:19 +0100

    Remove: remove old button stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,14 +3,3 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-
-import Button from '../components/common/Button';
-import '../components/common/Button/Button.scss';
-
-storiesOf('Button', module)
-  .add('default', () => <Button>Default Button</Button>)
-  .add('disabled', () => <Button disabled>Disabled Button</Button>)
-  .add('small', () => <Button size="small">Small Button</Button>)
-  .add('medium', () => <Button size="medium">Medium Button</Button>)
-  .add('big', () => <Button size="big">Big Button</Button>)
-  .add('full width', () => <Button fullWidth={true}>Full width Button</Button>);
