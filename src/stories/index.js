7d56a523b833a72ae059d7d48eb15d7c766560f3
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-13 02:04:39 +0100

    Added: new button cases stories

--- src/stories/index.js
+++ src/stories/index.js
@@ -9,4 +9,8 @@ import '../components/common/Button/Button.scss';
 
 storiesOf('Button', module)
   .add('default', () => <Button>Default Button</Button>)
-  .add('disabled', () => <Button disabled>Default Button</Button>);
+  .add('disabled', () => <Button disabled>Disabled Button</Button>)
+  .add('small', () => <Button size="small">Small Button</Button>)
+  .add('medium', () => <Button size="medium">Medium Button</Button>)
+  .add('big', () => <Button size="big">Big Button</Button>)
+  .add('full width', () => <Button fullWidth={true}>Full width Button</Button>);
