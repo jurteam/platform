7902745aec3460a96ae42a17dbac8db773d09cd1
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-14 13:47:33 +0100

    Added: added UserPreview to Header

--- src/components/common/Header/Header.stories.js
+++ src/components/common/Header/Header.stories.js
@@ -5,6 +5,7 @@ import { withInfo } from '@storybook/addon-info';
 
 import Header from './';
 import Logo from '../Logo';
+import UserPreview from '../UserPreview';
 
 storiesOf('Header', module)
   .addDecorator(withInfo)
@@ -15,4 +16,28 @@ storiesOf('Header', module)
     }
   })
   .add('Header', () => <Header />)
-  .add('Header width logo', () => <Header><Logo /></Header>);
+  .add('Logo', () => <Header><Logo /></Header>)
+  .add('UserPreview with name', () => (
+    <Header>
+      <Logo />
+      <UserPreview name="Alice" seed="0x3954939439487573664374" shouldRenderName balance="7546857" currency="Jur" />
+    </Header>
+  ))
+  .add('UserPreview without name', () => (
+    <Header>
+      <Logo />
+      <UserPreview name="" seed="0x3954939439487573664374" shouldRenderName balance="7546857" currency="Jur" />
+    </Header>
+  ))
+  .add('UserPreview with shouldrendername false', () => (
+    <Header>
+      <Logo />
+      <UserPreview name="Alice" seed="0x3954939439487573664374" balance="7546857" currency="Jur" />
+    </Header>
+  ))
+  .add('UserPreview with currency as number', () => (
+    <Header>
+      <Logo />
+      <UserPreview name="Alice" seed="0x3954939439487573664374" balance={2423} currency="Jur" />
+    </Header>
+  ));
