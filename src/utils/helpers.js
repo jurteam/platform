048ae9c863b82f814bcaafcf7378a8749aef6c93
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-14 15:58:23 +0100

    add helpers wip

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -8,3 +8,25 @@ export const log = (mixed, obj) => {
     }
   }
 };
+
+// EVM connection checker
+export const checkConnection = web3 => {
+  if (!web3) {
+    return { pathname: "/" };
+  }
+};
+
+// Route redirect helper
+export const redirect = (...checks) => {
+  log("redirect", { checks });
+  return (nextState, replace) => {
+    log("redirect", { nextState, replace });
+    let result;
+    checks.some(check => {
+      log("redirect", { check });
+      result = check();
+      return !!result;
+    });
+    result && replace(result);
+  };
+};
