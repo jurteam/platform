54a892df80721f74fa0269160daa453a48feed16
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-16 09:47:59 +0100

    Updated: set Amount component props type

--- src/components/common/Amount/Amount.js
+++ src/components/common/Amount/Amount.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import { toCurrencyFormat } from '../../../utils/helpers';
 
 import './Amount.scss';
 
 export const Amount = ({ value, currency }) => {
-  const currencyFallback = 'JUR';
-
   return (
     <span class="jur-amount">
-      { `${toCurrencyFormat(value)} ${currency ? currency.toUpperCase() : currencyFallback}` }
+      { `${toCurrencyFormat(value)} ${currency.toUpperCase()}` }
     </span>
   )
+};
+
+Amount.PropTypes = {
+  amount: PropTypes.number.isRequired,
+  currency: PropTypes.string
+};
+
+Amount.defaultProps = {
+  currency: 'JUR'
 };
