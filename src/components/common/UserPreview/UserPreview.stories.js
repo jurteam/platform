601bf9b9fd23e1de8a68e483e49dcd485077a53b
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-16 10:12:45 +0100

    Updated: Update UserPreview to use Amount component

--- src/components/common/UserPreview/UserPreview.stories.js
+++ src/components/common/UserPreview/UserPreview.stories.js
@@ -13,9 +13,8 @@ storiesOf('UserPreview', module)
       header: false
     }
   })
-  .add('should render name', () => <UserPreview name="Alice" seed="0x3954939439487573664374" shouldRenderName balance="7546857" currency="Jur" />)
-  .add('name unavailable', () => <UserPreview name="" seed="0x3954939439487573664374" shouldRenderName balance="7546857" currency="Jur" />)
-  .add('should render name is false', () => <UserPreview name="Alice" seed="0x3954939439487573664374" balance="7546857" currency="Jur" />)
-  .add('currency as number', () => <UserPreview name="Alice" seed="0x3954939439487573664374" balance={2423} currency="Jur" />)
+  .add('shouldRenderName is true', () => <UserPreview name="Alice" seed="0x3954939439487573664374" shouldRenderName balance="7546857" />)
+  .add('shouldRenderName is true and No name available', () => <UserPreview seed="0x3954939439487573664374" shouldRenderName balance="7546857" />)
+  .add('shouldRenderName is false', () => <UserPreview name="Alice" seed="0x3954939439487573664374" balance="7546857" />)
 
 
