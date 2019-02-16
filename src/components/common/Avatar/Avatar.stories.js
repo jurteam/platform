27f3162593df6cfc2946fcab994855ae24ea2967
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-16 10:42:41 +0100

    Update: update Avatar stories

--- src/components/common/Avatar/Avatar.stories.js
+++ src/components/common/Avatar/Avatar.stories.js
@@ -16,57 +16,36 @@ storiesOf('Avatar', module)
   .add('Avatar', () => (
     <div>
       <Avatar seed="0x70aec4b9cffa7b" size="xxsmall" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xsmall" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="small" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="medium" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="large" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xlarge" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xxlarge" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xxxlarge" />
     </div>
   ))
   .add('Avatar rounded', () => (
     <div>
       <Avatar seed="0x70aec4b9cffa7b" size="xxsmall" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xsmall" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="small" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="medium" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="large" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xlarge" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xxlarge" variant="rounded" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xxxlarge" variant="rounded" />
     </div>
   ))
   .add('Avatar circle', () => (
     <div>
       <Avatar seed="0x70aec4b9cffa7b" size="xxsmall" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xsmall" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="small" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="medium" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="large" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xlarge" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xxlarge" variant="circle" />
-      <br />
       <Avatar seed="0x70aec4b9cffa7b" size="xxxlarge" variant="circle" />
     </div>
   ));
