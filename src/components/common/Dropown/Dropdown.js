4cddbd5378b5666fa5cadba717b569ac52773628
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-17 10:50:50 +0100

    Updated: When click outside and Dropdown is opened, close it

--- src/components/common/Dropown/Dropdown.js
+++ src/components/common/Dropown/Dropdown.js
@@ -7,7 +7,26 @@ export class Dropdown extends Component {
     isOpen: false
   }
 
+  componentWillUpdate = (nextProps, nextState) => {
+    if (!this.state.isOpen && nextState.isOpen) {
+      document.addEventListener('click', this.onOutsideClick);
+    } else if (this.state.isOpen && !nextState.isOpen) {
+      document.removeEventListener('click', this.onOutsideClick);
+    }
+  }
+
+  componentWillUnmount = () => {
+    document.removeEventListener('click', this.onOutsideClick);
+  }
+
+  onOutsideClick = (ev) => {
+    let isClickInside = this.wrapperElRef.contains(ev.target);
+    if (!isClickInside) this.close();
+  }
+
   toggle = () => this.setState(state => ({ isOpen: !state.isOpen }))
+
+  close = () => this.setState({ isOpen: false })
 
   render() {
     let buttonEl = (
@@ -19,7 +38,10 @@ export class Dropdown extends Component {
       </button>
     );
     return (
-      <div className={`jur-dropdown ${this.state.isOpen ? 'jur-dropdown--open': ''}`}>
+      <div
+        className={`jur-dropdown ${this.state.isOpen ? 'jur-dropdown--open': ''}`}
+        ref={el => { this.wrapperElRef = el }}
+      >
         {buttonEl}
         {this.state.isOpen && 
           <div className="jur-dropdown__actions">
--- src/components/common/Dropown/Dropdown.stories.js
+++ src/components/common/Dropown/Dropdown.stories.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import centered from '@storybook/addon-centered';
 import { EllipsisVIcon } from '../Icons/EllipsisVIcon';
 
 import Dropdown from './';
@@ -9,19 +10,27 @@ import DropdownItem from '../DropdownItem';
 
 storiesOf('Dropdown', module)
   .addDecorator(withInfo)
+  .addDecorator(centered)
   .addParameters({ 
     info: { 
       inline: true,
       header: false
     }
   })
-  .add('Default', () => (
+  .add('Single action', () => (
     <Dropdown label={<EllipsisVIcon />}>
       <DropdownItem onClick={() => alert('DropdownItem handleClick fired')}>
         Archive
       </DropdownItem>
-      <DropdownItem onClick={() => alert('DropdownItem handleClick fired')}>
-        Archive
+    </Dropdown>
+  ))
+  .add('Multiple action', () => (
+    <Dropdown label={<EllipsisVIcon />}>
+      <DropdownItem onClick={() => alert('Action 1 fired')}>
+        Action 1
+      </DropdownItem>
+      <DropdownItem onClick={() => alert('Action 2 fired')}>
+        Action 2
       </DropdownItem>
     </Dropdown>
   ))
