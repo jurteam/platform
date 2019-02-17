5b0ec23486f60f884b8e31d8faceaa75da9fd996
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-17 22:14:39 +0100

    Updated: Fix tag

--- src/components/common/Tag/Tag.stories.js
+++ src/components/common/Tag/Tag.stories.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
-import Tag from './index';
+import Tag from './';
 
 storiesOf('Tag', module)
   .addDecorator(withInfo)
