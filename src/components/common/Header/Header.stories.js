8fbd54b12340c1278ae1f62c84997cc799882e1f
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-17 22:05:46 +0100

    Updated: Add MainNav to Header

--- src/components/common/Header/Header.stories.js
+++ src/components/common/Header/Header.stories.js
@@ -6,6 +6,7 @@ import { withInfo } from '@storybook/addon-info';
 import Header from './';
 import Logo from '../Logo';
 import ProfilePreview from '../ProfilePreview';
+import MainNav from '../MainNav';
 
 storiesOf('Header', module)
   .addDecorator(withInfo)
@@ -16,16 +17,17 @@ storiesOf('Header', module)
     }
   })
   .add('Header', () => <Header />)
-  .add('Logo', () => <Header><Logo /></Header>)
   .add('ProfilePreview with name', () => (
     <Header>
       <Logo />
+      <MainNav />
       <ProfilePreview name="Alice" seed="0x3954939439487573664374" shouldRenderName balance="7546857" currency="Jur" />
     </Header>
   ))
   .add('ProfilePreview without name', () => (
     <Header>
       <Logo />
+      <MainNav />
       <ProfilePreview name="" seed="0x3954939439487573664374" shouldRenderName balance="7546857" currency="Jur" />
     </Header>
   ))
