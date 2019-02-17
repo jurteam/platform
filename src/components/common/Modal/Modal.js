48a75c7a782d80b78cabbcdac45c5318d1d515d6
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-17 12:52:12 +0100

    Added: Modal component logic

---
+++ src/components/common/Modal/Modal.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ReactModal from 'react-modal';
+
+import './Modal.scss';
+
+export class Modal extends Component {
+  render() {
+    const {
+      isOpen,
+      onRequestClose,
+      onAfterOpen,
+      className,
+      ...props
+    } = this.props;
+    return (  
+      <ReactModal
+        isOpen={ isOpen }
+        ariaHideApp={ false }
+        shouldCloseOnEsc={ true }
+        onRequestClose={ onRequestClose }
+        onAfterOpen={ onAfterOpen }
+        className={ `jur-modal ${this.props.className || ''}` }
+        overlayClassName='jur-modal__overlay'
+        { ...props }
+      >
+        { this.props.children }
+      </ReactModal>
+    )
+  }
+}
+
+Modal.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  onRequestClose: PropTypes.func,
+  onAfterOpen: PropTypes.func,
+  className: PropTypes.string
+};
