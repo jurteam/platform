62d38072aa6ae2d57ee4989a67111d3799b9b2ad
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-18 11:26:44 +0100

    update contracts and disputes components with breadcrumbs

--- src/components/sections/Contracts/Contracts.js
+++ src/components/sections/Contracts/Contracts.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 
+import Breadcrumbs from "./../../../components/common/Breadcrumbs";
+
 export class Contracts extends Component {
-
-    render() {
-
-        return <div className="jur--body">Contracts Section</div>;
-    }
+  render() {
+    return (
+      <div className="jur--body">
+        <Breadcrumbs />
+        Contracts Section
+      </div>
+    );
+  }
 }
--- src/components/sections/Disputes/Disputes.js
+++ src/components/sections/Disputes/Disputes.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 
+import Breadcrumbs from "./../../../components/common/Breadcrumbs";
+
 export class Disputes extends Component {
-
-    render() {
-
-        return <div className="jur--body">Disputes Section</div>;
-    }
+  render() {
+    return (
+      <div className="jur--body">
+        <Breadcrumbs />
+        Disputes Section
+      </div>
+    );
+  }
 }
