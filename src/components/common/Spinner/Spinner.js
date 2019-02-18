5b3cc7f93f00a5d512621545e03818c8fb897f0d
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-18 19:08:16 +0100

    update spinner behavior due new app loading

--- src/components/common/Spinner/Spinner.js
+++ src/components/common/Spinner/Spinner.js
@@ -4,8 +4,8 @@ import React from "react";
 import src from "../../../assets/loading.gif";
 import style from "./Spinner.scss"; // load scss properly
 
-export const Spinner = ({ app: { loaded } }) => (
-  <div className={`jur--spinner${loaded ? " off" : ""}`}>
+export const Spinner = ({ app: { loading } }) => (
+  <div className={`jur--spinner${loading ? "" : " off"}`}>
     <img src={src} alt="Loading..." />
   </div>
 );
