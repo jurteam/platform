89ea85b7521f8872ea0148e5f809dc1be26196b2
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-18 02:31:01 +0100

    Added: Accordion Item component

---
+++ src/components/common/AccordionItem/AccordionItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export const AccordionItem = ({ active, title, children, onClick }) => (
+  <li className={`jur-accordion__item ${active ? 'jur-accordion__item--active' : ''}`}>
+    <div className="jur-accordion__item__header" onClick={ () => onClick() }>{ title }</div>
+    <div className="jur-accordion__item__content">{ children }</div>
+  </li>
+);
