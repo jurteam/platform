a49f054abef9cf2fc80609b519c05d91cbceb2ac
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-18 19:58:10 +0100

    Updated: Refact MetaMask => UserAction

--- src/components/common/MetaMaskAlert/index.js
+++
@@ -1,2 +0,0 @@
-import { MetaMaskAlert } from './MetaMaskAlert';
-export default MetaMaskAlert;
---
+++ src/components/common/UserAction/UserAction.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+import { AlertIcon } from '../Icons/AlertIcon';
+
+import './UserAction.scss';
+
+export const UserAction = ({ children, className }) => {
+  return (
+    <div className={ `jur-user-action ${ className || ''}`}>
+      { children }
+    </div>
+  );
+}
+
+UserAction.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
