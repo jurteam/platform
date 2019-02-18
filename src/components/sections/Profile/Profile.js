d1c9887c84e380a5a325e612b5db55b89b467e74
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-18 11:12:58 +0100

    update profile main section component due inner sections

--- src/components/sections/Profile/Profile.js
+++ src/components/sections/Profile/Profile.js
@@ -1,9 +1,36 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
+import { withRouter } from "react-router";
 
-export class Profile extends Component {
+import { ProfileContext } from "./";
 
-    render() {
+import Breadcrumbs from "./../../../components/common/Breadcrumbs";
+import Side from "./../../../components/common/Side";
 
-        return <div className="jur--body">Profile Section</div>;
-    }
-}
+import ProfileSettings from "./ProfileSettings";
+
+const Profile = props => {
+  const context = useContext(ProfileContext);
+  const { navigation } = context;
+  const {
+    location: { pathname }
+  } = props;
+
+  const showContent = () => {
+    const currentContent = navigation.profile.find(el => {
+      return el.path === pathname;
+    });
+
+    // fallback to profile settings by default
+    return currentContent.component || <ProfileSettings />;
+  };
+
+  return (
+    <div className="jur--body jur--body-sided">
+      <Breadcrumbs />
+      <Side items={navigation.profile} />
+      <div className="jur--inner">{showContent()}</div>
+    </div>
+  );
+};
+
+export default withRouter(Profile);
--- src/components/sections/Profile/index.js
+++ src/components/sections/Profile/index.js
@@ -1,4 +1,39 @@
-// import { connect } from "react-redux";
-import { Profile } from "./Profile";
+import React, { Component } from "react";
 
-export default Profile
+import Profile from "./Profile";
+
+// Inner pages
+import ProfileSettings from "./ProfileSettings";
+import Privacy from "./Privacy";
+import Faq from "./Faq";
+import Notification from "./Notification";
+import Terms from "./Terms";
+
+import i18n from "./../../../assets/i18n/en.json"; // i18n
+
+// Section Context
+export const ProfileContext = React.createContext();
+
+class ProfileProvider extends Component {
+  state = {
+    navigation: {
+      profile: [
+        { label: i18n.profileSettings, path: "/profile", component: <ProfileSettings /> },
+        { label: i18n.privacy, path: "/profile/privacy", component: <Privacy /> },
+        { label: i18n.notification, path: "/profile/notifications", component: <Notification /> },
+        { label: i18n.faq, path: "/profile/faq", component: <Faq/> },
+        { label: i18n.termOfService, path: "/profile/terms", component: <Terms /> }
+      ]
+    }
+  };
+
+  render() {
+    return (
+      <ProfileContext.Provider value={this.state}>
+        <Profile />
+      </ProfileContext.Provider>
+    );
+  }
+}
+
+export default ProfileProvider;
