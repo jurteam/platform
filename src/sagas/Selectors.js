59d9f34adc793d687fc0a14a29d6564dadaa7002
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-18 19:08:30 +0100

    add sagas selectors

---
+++ src/sagas/Selectors.js
@@ -0,0 +1,11 @@
+// Network
+export const getNetwork = state => state.web3
+
+// Connector
+export const getDrizzleStatus = state => state.drizzleStatus
+export const getContracts = state => state.contracts
+export const getJURToken = state => state.contracts.JURToken
+
+// Wallet
+export const getAccounts = state => state.accounts
+export const getWallet = state => state.wallet
