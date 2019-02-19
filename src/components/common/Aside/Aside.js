4dc8a95733ddc68b41ccde33cce47c90cdfc44ad
Salmen <salmen.bejaoui@gmail.com>
2019-02-19 12:04:16 +0100

    Added: Aside component

---
+++ src/components/common/Aside/Aside.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Aside.scss';
+
+export const Aside = ({ children }) => (
+  <aside class="jur-aside">{ children }</aside>
+);
