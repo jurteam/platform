7600932affe8539f3721329b96d0c5428ebd6641
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-19 06:15:39 +0100

    Updated: Fix TableCell and TableRow

--- src/components/common/TableCell/TableCell.js
+++ src/components/common/TableCell/TableCell.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
 import PropsTypes from 'prop-types';
 
-export const TableCell = ({ className, parentComponent, children }) => {
+export const TableCell = ({ className, parentComponent, children, onClick }) => {
   const [desc, setDesc] = useState(false);
   const handleClick = ev => {
-    this.props.onClick.bind(this, ev, desc)();
+    onClick.bind(this, ev, desc)();
     setDesc(!desc);
   }
   const Component = parentComponent === 'thead' ? 'th' : 'td';
--- src/components/common/TableRow/TableRow.js
+++ src/components/common/TableRow/TableRow.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 export const TableRow = ({ parentComponent, children }) => {
   const childrenArray = React.Children.toArray(children);
   const cells = childrenArray.map(cell => (
-    React.cloneElement(cell, { parentComponent, ...this.props })
+    React.cloneElement(cell, { parentComponent, ...cell.props })
   ));
   return <tr>{ cells }</tr>;
 }
