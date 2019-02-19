47047be3b21f0e4950100e2a1d7fab72730e9dfd
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-19 06:15:58 +0100

    Updated: Refact TableBody

--- src/components/common/TableBody/TableBody.js
+++ src/components/common/TableBody/TableBody.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-export const TableBody = ({ children }) => {
-  return <tbody>{ children }</tbody>;
-};
+export const TableBody = ({ children }) => (
+  <tbody>{ children }</tbody>
+);
