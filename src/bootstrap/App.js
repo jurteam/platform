9678c2e4019792fb4599d4aa087f8703d2003538
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-19 19:15:35 +0100

    Updated: refactoring App main component

--- src/bootstrap/App.js
+++ src/bootstrap/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from "react";
 
-// Context
-import AppProvider from "./AppProvider";
+import AppProvider from "./AppProvider"; // App Context
 
-// Initializer
-import Initializer from "./Initializer";
+import ErrorBoundary from "./ErrorBoundary"; // Errors
 
 // Drizzle
 import { DrizzleProvider } from "drizzle-react";
 import drizzleOptions from "./../config/drizzleOptions";
+
+// Components
+import Spinner from "../components/common/Spinner";
+
+import UnderAuth from "./UnderAuth";
+import Initializer from "./Initializer"; // Initializer
 
 class App extends Component {
   render() {
     const { store, history, testElement } = this.props;
 
     return (
-      <AppProvider>
-        <DrizzleProvider options={drizzleOptions} store={store}>
-          <Initializer history={history} testElement={testElement} />
-        </DrizzleProvider>
+      <AppProvider store={store}>
+        <ErrorBoundary>
+          <>
+            <Spinner store={store} />
+            <UnderAuth>
+              <DrizzleProvider options={drizzleOptions} store={store}>
+                <Initializer history={history} testElement={testElement} />
+              </DrizzleProvider>
+            </UnderAuth>
+          </>
+        </ErrorBoundary>
       </AppProvider>
     );
   }
