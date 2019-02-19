18182ab3171aeb6249188cd8d837e8fd28c00981
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-19 05:58:17 +0100

    Added: TableRow component

---
+++ src/components/common/TableRow/TableRow.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export const TableRow = ({ parentComponent, children }) => {
+  const childrenArray = React.Children.toArray(children);
+  const cells = childrenArray.map(cell => (
+    React.cloneElement(cell, { parentComponent, ...this.props })
+  ));
+  return <tr>{ cells }</tr>;
+}
