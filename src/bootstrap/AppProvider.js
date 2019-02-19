cd7feac8d13725d9ee6f8dd923a868bfa09586f0
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-19 19:14:53 +0100

    Updated: AppProvider context due Metamask login flow

--- src/bootstrap/AppProvider.js
+++ src/bootstrap/AppProvider.js
@@ -3,14 +3,62 @@ import React, { Component } from "react";
 import appReference from "./../../package.json"; // config
 import i18n from "./../assets/i18n/en.json"; // i18n
 
+// MetaMask
+import MetaMask from "../hooks/MetaMask";
+import { SET_LOADING } from "../reducers/types.js";
+
 // Application Context
 export const AppContext = React.createContext();
 
 class AppProvider extends Component {
-  state = {
-    version: appReference.version,
-    labels: i18n
-  };
+  constructor(props) {
+    super(props);
+    this.auth = this.auth.bind(this);
+    this.exit = this.exit.bind(this);
+
+    this.store = props.store;
+
+    this.state = {
+      version: appReference.version,
+      labels: i18n,
+      metamaskLoading: true,
+      onNetwork: false,
+      auth: this.auth,
+      exit: this.exit
+    };
+  }
+
+  componentDidMount() {
+    if(
+    typeof window.web3 === 'object') {
+    this.auth();
+    } else {
+      this.setState({metamaskLoading: false})
+      this.store.dispatch({ type: SET_LOADING, payload: false });
+    }
+  }
+
+  auth() {
+    this.store.dispatch({ type: SET_LOADING, payload: true });
+    this.setState({ metamaskLoading: true });
+    MetaMask.auth(
+      () => {
+        // success
+        console.log("AppProvider", "store should be updated");
+        this.setState({ onNetwork: true });
+      },
+      () => {
+        // error
+        console.log("AppProvider", "network connection error!");
+        this.setState({ metamaskLoading: false });
+        this.store.dispatch({ type: SET_LOADING, payload: false });
+      }
+    );
+  }
+
+  exit() {
+    this.setState({ onNetwork: false });
+  }
 
   render() {
     return (
