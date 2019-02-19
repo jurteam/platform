f9cf01a054c965dd2e945031992f6a1ee524a692
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-19 21:22:27 +0100

    Added: UserPrivacy Component

---
+++ src/components/common/UserPrivacy/UserPrivacy.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import UserAction from '../UserAction';
+import UserActionHeader from '../UserActionHeader';
+import UserActionBody from '../UserActionBody';
+import UserActionFooter from '../UserActionFooter';
+import Button from '../Button';
+
+import './UserPrivacy.scss';
+
+export const UserPrivacy = ({ data, className }) => (
+  <div className={`jur-user-privacy ${className || ''}`}>
+    {data.map((item, i) => (
+      <UserAction key={i.toString()}>
+        <UserActionHeader>
+          { item.title }
+        </UserActionHeader>
+        <UserActionBody>
+          { item.description }
+        </UserActionBody>
+        <UserActionFooter>
+          <Button size="big" onClick={ () => item.handler() }>
+            { item.buttonLabel }
+          </Button>
+        </UserActionFooter>
+      </UserAction>
+    ))}
+  </div>
+);
