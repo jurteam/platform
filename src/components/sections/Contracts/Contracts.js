4d5622a7f0a0f3980337e50e0ab5adc7685411c0
Marco Marasco <marco.marasco@pleaseup.com>
2019-02-21 13:10:53 +0100

    Updated: contracts home integration wip

--- src/components/sections/Contracts/Contracts.js
+++ src/components/sections/Contracts/Contracts.js
@@ -7,21 +7,43 @@ import { AppContext } from "../../../bootstrap/AppProvider";
 import PageLayout from "../../../components/common/PageLayout";
 import Main from "../../../components/common/Main";
 import Aside from "../../../components/common/Aside";
+import ContractsTable from "../../../components/common/ContractsTable";
+
+import { log } from "../../../utils/helpers";
+import { NEW_ARBITRATION } from "../../../reducers/types";
 
 export const Contracts = () => {
   const { labels } = useContext(AppContext);
 
+  const handleArchive = () => {
+    log("Contracts", "handleArchive");
+  };
+
+  const newArbitration = () => {
+    log("newArbitration", "run");
+    global.drizzle.store.dispatch({ type: NEW_ARBITRATION });
+  };
+
   const breadcrumbs = [
     {
       label: labels.contracts,
-      active: true
+      active: true,
+      to: "/contracts"
     }
   ];
 
   return (
     <PageLayout breadcrumbs={breadcrumbs}>
-      <Main>Contracts Section</Main>
-      <Aside>Contracts Aside</Aside>
+      <ContractsTable
+        headers={[
+          { label: "Status", sortable: false },
+          { label: "Contract Name", sortable: false },
+          { label: "Duration", sortable: false }
+        ]}
+        data={[]}
+        handleArchive={handleArchive}
+        newContract={newArbitration}
+      />
     </PageLayout>
   );
 };
