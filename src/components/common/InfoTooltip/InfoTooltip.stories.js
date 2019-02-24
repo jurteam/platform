f7951c88485c7f1dfa4540426f1ad3708b0eb19d
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-02-24 21:36:08 +0100

    Added: Tooltip component

---
+++ src/components/common/InfoTooltip/InfoTooltip.stories.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import centered from '@storybook/addon-centered';
+
+import InfoTootip from './';
+
+storiesOf('InfoTooltip', module)
+  .addDecorator(withInfo)
+  .addDecorator(centered)
+  .addParameters({ 
+    info: { 
+      inline: true,
+      header: false
+    }
+  })
+  .add('Without text', () => <InfoTootip />)
+  .add('With text Top', () => 
+    <InfoTootip
+      position="top"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel laoreet augue, eget pellentesque dui posuere"
+    />
+  )
+  .add('With text right', () => 
+    <InfoTootip
+      position="right"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel laoreet augue, eget pellentesque dui posuere"
+    />
+  )
+  .add('With text bottom', () => 
+    <InfoTootip
+      position="bottom"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel laoreet augue, eget pellentesque dui posuere"
+    />
+  )
+  .add('With text left', () => 
+    <InfoTootip
+      position="left"
+      text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel laoreet augue, eget pellentesque dui posuere"
+    />
+  )
