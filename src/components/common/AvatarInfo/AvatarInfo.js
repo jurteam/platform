1ecb6c08ea23ae5338dc7e3acf009d4b7a8fa273
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-03-03 20:54:13 +0100

    Added: ContractCounterparties component

--- src/components/common/AvatarInfo/AvatarInfo.js
+++ src/components/common/AvatarInfo/AvatarInfo.js
@@ -4,11 +4,11 @@ import Avatar from '../Avatar';
 
 import './AvatarInfo.scss';
 
-export const AvatarInfo = ({ userName, userWallet, renderName, variant, maxTextWidth }) => (
+export const AvatarInfo = ({ userName, userWallet, shouldRenderName, variant, maxTextWidth }) => (
   <div className={`jur-avatar-info ${variant ? `jur-avatar-info--${variant}` : ''}`}>
     <Avatar seed={ userWallet } size="xsmall" />
     <div className="jur-avatar-info__text" style={{width: maxTextWidth || 150}}>
-      { renderName && userName ?
+      { shouldRenderName && userName ?
           userName
         : userWallet
       }
---
+++ src/components/common/ContractCounterparties/ContractCounterparties.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BlockTitle from '../BlockTitle';
+import AvatarInfo from '../AvatarInfo';
+
+import './ContractCounterparties.scss';
+
+export const ContractCounterparties = ({counterparties, CounterpartiesDescription}) => (
+  <div className="jur-contract-counterparties">
+    <BlockTitle title="Counterparties" description={CounterpartiesDescription} />
+    <div className="jur-contract-counterparties__value">
+      {
+        counterparties.map(counterparty => (
+          <AvatarInfo
+            userName={counterparty.name}
+            userWallet={counterparty.wallet.address}
+            shouldRenderName={counterparty.shouldRenderName}
+            variant={counterparty.shouldRenderName ? counterparty.name ? null : 'ellipsis' : 'ellipsis'}
+          />
+        ))
+      }
+      <AvatarInfo
+      />
+    </div>
+  </div>
+)
