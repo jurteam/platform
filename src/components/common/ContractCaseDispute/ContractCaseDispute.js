0a907a7ef80066f4dd8ae0cc98be6f1595efa48a
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-03-03 21:44:37 +0100

    Added: ContractCaseDispute component

---
+++ src/components/common/ContractCaseDispute/ContractCaseDispute.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import BlockTile from '../BlockTitle';
+
+import './ContractCaseDispute.scss';
+
+export const ContractCaseDispute = ({selectedCase, disputeDescription}) => (
+  <div className="jur-contract-case-dispute">
+    <BlockTile title="In Case of Dispute:" description={disputeDescription} />
+    { selectedCase &&
+      <div className="jur-contract-case-dispute__value">{selectedCase.label}</div>
+    }
+  </div>
+);
