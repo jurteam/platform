256f1b53fa277c71b56c276479166af0a7d8b0ed
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-03-03 19:27:30 +0100

    Added: Contract Name component

--- .storybook/config.js
+++ .storybook/config.js
@@ -5,7 +5,7 @@ import StoryRouter from 'storybook-react-router';
 addDecorator(StoryRouter());
 
 const req = requireContext('../src/components', true, /\.stories\.js$/);
-
+require('../src/assets/scss/_base.scss');
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
---
+++ src/components/common/ContractName/ContractName.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import ContractName from './';
+
+storiesOf('ContractName', module)
+  .addDecorator(withInfo)
+  .addParameters({ 
+    info: { 
+      inline: true,
+      header: false
+    }
+  })
+  .add('Default', () => (
+    <ContractName
+      contractName="Contract name"
+      onContractNameChange={ev => console.log(ev.target.value) }
+      statusId={0}
+      statusIdLabel="Draft"
+    />
+  ))
