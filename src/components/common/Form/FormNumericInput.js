a961c11f8ae387d4636c889546b5537a10e7317d
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-03-04 20:59:19 +0100

    Updated: Fix FormNumericInput

--- src/components/common/Form/FormNumericInput.js
+++ src/components/common/Form/FormNumericInput.js
@@ -5,12 +5,28 @@ import {CaretUpIcon} from '../Icons/CaretUpIcon';
 
 export const FormNumericInput = ({label, initialValue, onChange}) => {
   const [value, setValue] = useState(initialValue || 0);
-  const plusOne = () => setValue(Number(value) + 1);
-  const minusOne = () => setValue(Number(value) === 0 ? 0 : Number(value) - 1);
+  const plusOne = () => {
+    setValue(Number(value) + 1);
+    onChange(value);
+  };
+  const minusOne = () => {
+    setValue(Number(value) === 0 ? 0 : Number(value) - 1)
+    onChange(value);
+  };
+  const handleChange = value => {
+    if (typeof value === 'number') {
+      onChange(value);
+      return;
+    }
+    const newValue = Number(value.target.value);
+    setValue(newValue);
+    onChange(value);
+  };
+
   return (
     <div className="jur-form__numeric-input">
       <div>
-        <input type="number" value={value} onChange={ev => onChange(ev.target.value)}/>
+        <input type="number" value={value} onChange={handleChange}/>
         <label>{label}</label>
       </div>
       <div className="jur-form__numeric-input__actions">
