c2fdda9bc33d97812fb612f3eb50b62aa88caf0f
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-03-04 20:09:52 +0100

    Added: ContractSelectCategory Component

---
+++ src/components/common/ContractSelectCategory/ContractSelectCategory.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import categories from '../../../assets/categories.json';
+import Form from '../Form';
+
+import './ContractSelectCategory.scss';
+
+export const ContractSelectCategory = ({onChange}) => (
+  <div className="jur-contract-select-category">
+    <div className="jur-contract-select-category__title">Category:</div>
+    <Form.Select
+      options={categories}
+      onChange={ev => onChange(ev)}
+    />
+  </div>
+)
---
+++ src/components/common/ContractSelectCategory/ContractSelectCategory.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import ContractSelectCategory from './';
+
+storiesOf('ContractSelectCategory', module)
+  .addDecorator(withInfo)
+  .addParameters({ 
+    info: { 
+      inline: true,
+      header: false
+    }
+  })
+  .add('Default', () => <ContractSelectCategory onChange={ev => console.log(ev)} />)
