8ed68b635952b4322c02cee8d18db1c91fc22f08
Marco Marasco <marco.marasco@pleaseup.com>
2019-03-05 10:27:12 +0100

    Update helpers refactor

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,25 +1,66 @@
+import anchorme from "anchorme";
 
-import anchorme from 'anchorme';
-
-// Log helper only on DEVELOPMENT environment
-export const log = (mixed, obj) => {
+// Log helper only on DEVELOPMENT environmentexport const warn = (mixed, obj) => {
+export const warn = (mixed, obj) => {
+  return log(mixed, obj, 1);
+};
+export const error = (mixed, obj) => {
+  return log(mixed, obj, -1);
+};
+export const log = (mixed, obj, type) => {
+  let out = [];
+  if (typeof type === "undefined") type = 0; // default to log
   if (process.env.NODE_ENV === "development") {
     if (typeof obj === "undefined") {
-      console.log(mixed);
+      out = [mixed];
     } else {
-      console.log(mixed, obj);
+      out = [mixed, obj];
     }
+  }
+  switch (type) {
+    case -1: // error
+      console.error(...out);
+      break;
+    case 1: // warn
+      console.warn(...out);
+      break;
+    default:
+      // log
+      console.log(...out);
+      break;
+  }
+};
+// EVM connection checker
+export const checkConnection = web3 => {
+  if (!web3) {
+    return { pathname: "/" };
   }
 };
 
-export const toCurrencyFormat = (value) => {
+// Route redirect helper
+export const redirect = (...checks) => {
+  log("redirect", { checks });
+  return (nextState, replace) => {
+    log("redirect", { nextState, replace });
+    let result;
+    checks.some(check => {
+      log("redirect", { check });
+      result = check();
+      return !!result;
+    });
+    result && replace(result);
+  };
+};
+
+export const toCurrencyFormat = value => {
   var num = Number(value) / 100;
-  var res = num.toString().split('.');
+  var res = num.toString().split(".");
   if (res.length === 1 || res[1].length < 2) return num.toFixed(2);
   return num;
 };
 
-export const capitalize = string => (string.charAt(0).toUpperCase() + string.slice(1));
+export const capitalize = string =>
+  string.charAt(0).toUpperCase() + string.slice(1);
 
 export const urlify = str => {
   return anchorme(str);
