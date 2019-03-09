908369cfa84f051b6d41165516916856bb7bccf4
Salmen Bejaoui <salmen.bejaoui@gmail.com>
2019-03-09 15:15:01 +0100

    Added: BlockInfo component

---
+++ src/components/common/BlockInfo/BlockInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {InfoIcon} from '../Icons/InfoIcon';
+
+import './BlockInfo.scss';
+
+export const BlockInfo = ({title, description}) => (
+  <div className="jur-block-info">
+    <InfoIcon />
+    <span>
+      {title &&
+        <strong>{title}: </strong>
+      }
+      <span>{description}</span>
+    </span>
+  </div>
+);
