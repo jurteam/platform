1df73f6d91aeec44a337c2467b2eb4872ed0e0fe
Matteo <mdpproduction@gmail.com>
2019-03-11 16:46:57 +0100

    Updated: fix namespace

--- rest/app/Models/Contract.php
+++ rest/app/Models/Contract.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Models\Trait\StatusesTrait;
+use App\Models\Traits\StatusesTrait;
 use App\Models\Traits\ActivitiesTrait;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia\HasMedia;
