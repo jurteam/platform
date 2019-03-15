02f7e476aa8bc4661604739fa9d0ba6e13a9fcb0
Marco Marasco <marco.marasco@pleaseup.com>
2019-03-15 01:08:54 +0100

    Update storybook config update due store integration

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,35 @@
-import { configure, addDecorator } from '@storybook/react';
-import requireContext from 'require-context.macro';
-import StoryRouter from 'storybook-react-router';
+import React from "react";
+import { configure, addDecorator } from "@storybook/react";
+import requireContext from "require-context.macro";
+
+import StoryRouter from "storybook-react-router";
+import AppProvider from "../src/bootstrap/AppProvider";
+import ErrorBoundary from "../src/bootstrap/ErrorBoundary";
+
+// Drizzle
+import { DrizzleProvider } from "drizzle-react";
+import drizzleOptions from "../src/config/drizzleOptions";
+
+import configureStore from "../src/bootstrap/Store";
+// destructuring store config
+const { store, history } = configureStore();
 
 addDecorator(StoryRouter());
 
-const req = requireContext('../src/components', true, /\.stories\.js$/);
-require('../src/assets/scss/_variables.scss');
-require('../src/assets/scss/_fonts.scss');
-require('../src/assets/scss/_base.scss');
+addDecorator(fn => (
+  <AppProvider store={store}>
+    <ErrorBoundary>
+      <DrizzleProvider options={drizzleOptions} store={store}>
+        {fn()}
+      </DrizzleProvider>
+    </ErrorBoundary>
+  </AppProvider>
+));
+
+const req = requireContext("../src/components", true, /\.stories\.js$/);
+require("../src/assets/scss/_variables.scss");
+require("../src/assets/scss/_fonts.scss");
+require("../src/assets/scss/_base.scss");
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
