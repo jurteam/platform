dd2d2e83833613c03a35a559a7f2e5628a3cfb37
Matteo <mdpproduction@gmail.com>
2019-03-18 14:06:38 +0100

    Updated: remove record from media on delete entity

--- rest/app/Models/Traits/UploadableTrait.php
+++ rest/app/Models/Traits/UploadableTrait.php
@@ -4,6 +4,16 @@ namespace App\Models\Traits;
 
 trait UploadableTrait
 {
+    public static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function($model) {
+            $model->clearMediaCollection('evidences');
+            $model->clearMediaCollection('attachments');
+        });
+    }
+
     public function uploadMedia($params, $collection = 'attachments')
     {
         foreach ($params->attachments as $attachment) {
