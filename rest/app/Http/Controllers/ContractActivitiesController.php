789901ad60cb9e28cdabe84cd74138707902de53
Matteo <mdpproduction@gmail.com>
2019-04-23 09:48:50 +0200

    Updated: fix activities

--- rest/app/Http/Controllers/ContractActivitiesController.php
+++ rest/app/Http/Controllers/ContractActivitiesController.php
@@ -38,7 +38,7 @@ class ContractActivitiesController extends Controller
                             ->oldest('created_at')
                             ->get();
 
-        return $this->response->paginator($activities, new ContractActivityTransformer);
+        return $this->response->collection($activities, new ContractActivityTransformer);
     }
 
     /**
