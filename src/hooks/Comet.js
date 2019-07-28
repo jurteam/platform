a40e29e280fb0d5f4e2a1c07411a3c1d4899798b
Marco Marasco <marco.marasco@pleaseup.com>
2019-07-28 21:44:19 +0200

    Added comet class

---
+++ src/hooks/Comet.js
@@ -0,0 +1,100 @@
+/* eslint-disable no-unused-vars */
+import { log } from "../utils/helpers"; // log helpers
+
+import { thorify } from 'thorify'
+import { extend } from 'thorify/dist/extend'
+const Web3 = require("web3"); // Recommend using require() instead of import here
+
+// Comet Handler
+
+class CometProvider {
+  static provider = null;
+  static enableCall = null;
+
+  constructor() {
+
+    console.log('Before init – this.web3 [before]', this.web3);
+
+    // Checking if Thor has been injected by the browser
+    if (typeof this.thor !== 'undefined') {
+      // Use thor provider
+      this.web3 = new Web3(this.thor);
+      // Extend web3 to connect to VeChain Blockchain
+      extend(this.web3)
+    } else {
+      // Fall back to default thorified construction
+      this.web3 = thorify(new Web3(), "http://localhost:8669");
+    }
+
+    window.web3 = this.web3;
+
+    console.log('Before init – this.web3 [after]', this.web3);
+
+    if (window.web3) {
+      this.provider = window.web3.currentProvider;
+      log("CometProvider - constructor", this.provider);
+    }
+  }
+
+  setProvider(provider) {
+    this.provider = provider;
+    log("CometProvider - provider", provider);
+  }
+
+  // ask for approve
+  async auth(onSuccess, onError) {
+    // const { enable } = this.provider;
+    // eslint-disable-next-line no-undef
+    const { enable } = thor;
+    log("CometProvider", "enable()");
+
+    this.enableCall = await enable()
+      .then(res => {
+        log("CometProvider - enable then", res);
+        log("CometProvider - wallet address", res.pop());
+        if (typeof onSuccess === "function") {onSuccess(res);}
+
+        return this.provider
+      })
+      .catch(err => {
+        log("CometProvider - enable catch", err);
+        if (typeof onError === "function") {onError(err);}
+        return err
+      }); // promise
+
+    return this.enableCall;
+  }
+
+  // enabled checker
+  isEnabled() {
+    const { _comet } = this.provider;
+    return _comet.isEnabled();
+  }
+
+  // approved checker
+  isApproved() {
+    const { _comet } = this.provider;
+    return _comet.isApproved();
+  }
+
+  // unlocked checker
+  isUnlocked() {
+    const { _comet } = this.provider;
+    return _comet.isUnlocked();
+  }
+
+  // stops all operations
+  cancel() {
+
+    // end enable call if defined
+    if (
+      typeof this.enableCall !== "undefined" &&
+      typeof this.enableCall.then === "function"
+    )
+      this.enableCall.reject();
+  }
+}
+
+const Comet = new CometProvider();
+
+export default Comet;
