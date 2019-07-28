1d0042c85d2a52dfb410fadb3370d741cb856680
Marco Marasco <marco.marasco@pleaseup.com>
2019-07-28 17:03:34 +0200

    Updated custom provider for Drizzle

--- src/config/drizzleOptions.js
+++ src/config/drizzleOptions.js
@@ -4,9 +4,15 @@ import Arbitration from "../build/contracts/Arbitration.json";
 import ArbitrationFactory from "../build/contracts/ArbitrationFactory.json";
 import JURToken from "../build/contracts/JURToken.json";
 
+import { thorify } from "thorify";
+const Web3 = require("web3");		// Recommend using require() instead of import here
+
+const customProvider = thorify(new Web3(), "http://localhost:8669");
+
 const options = {
   web3: {
     block: false,
+    customProvider,
     fallback: {
       type: "ws",
       url: "ws://127.0.0.1:8545"
