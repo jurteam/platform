c921676d444d1b0909730377a7aabfa8f6e85ec1
Marco Marasco <marco.marasco@pleaseup.com>
2019-09-04 12:16:14 +0200

    Added provisioning bash script wip

---
+++ bin/provisioning.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# please have a look see here https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-ubuntu-18-04
+
+# 0. Install Nginx – Already installed
+# 1. Install MySQL
+# sudo apt update
+# sudo apt-get install -y mysql-server
+
+# sudo apt install -y php-dom
+
+# x. Install Composer
+# sudo apt update
+# sudo apt install -y curl php-cli php-mbstring git unzip
+# cd ~
+# curl -sS https://getcomposer.org/installer -o composer-setup.php
+# sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
