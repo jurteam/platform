b493d16c1eb0a65b8246b7187dda297efaf6340c
Marco Marasco <marco.marasco@pleaseup.com>
2019-10-22 19:58:30 +0200

    Updated added sendgrid

--- rest/bootstrap/app.php
+++ rest/bootstrap/app.php
@@ -108,6 +108,12 @@ $app->alias('mailer', Illuminate\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
 $app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
 
+$app->configure('mail');
+$app->configure('services');
+$app->register(Sichikawa\LaravelSendgridDriver\MailServiceProvider::class);
+
+unset($app->availableBindings['mailer']);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
