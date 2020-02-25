65d4e2d0872d95e5b6964d4e6c1ff20072274b66
Fernando Rugge <fernando.rugge@pleaseup.com>
2020-02-25 17:59:13 +0100

    updated mail text fixes

--- rest/resources/views/emails/contracts/ongoing/reached-deadline.blade.php
+++ rest/resources/views/emails/contracts/ongoing/reached-deadline.blade.php
@@ -2,7 +2,7 @@
 # Contract Closing
 
 <p>Hey {{$party['name']}},<br>
-your contract {{$contract->address}} has reached its expiration date on {{$expirationDate->formatLocalized('%d %b %Y')}}.<br>We hope that everything went good with your counterparty. If this is not the case kindly press "Success" button in order to send the closing proposal to your counterparty. If you had any problem, <strong>you can open a friendly resolution or a dispute</strong> through the platform. You just need to access your contract and press the "<i>Dispute</i>" button. <a href="{{$url}}" class="button button-inline button-jur" target="_blank">Go to contract</a></p>
+your contract {{$contract->address}} has reached its expiration date on {{$expirationDate->formatLocalized('%d %b %Y')}}.<br>We hope that everything went good with your counterparty. If this is the case kindly press the "Success" button in order to send the closing proposal to your counterparty. If you had any problem, <strong>you can open a friendly resolution or a dispute</strong> through the platform. You just need to access your contract and press the "<i>Dispute</i>" button. <a href="{{$url}}" class="button button-inline button-jur" target="_blank">Go to contract</a></p>
 
 Best,<br>
 {{ config('jur.name') }}
