8f5eed4c56e6b6a1a81ad6b1917ef80a26370b7e
Fernando Rugge <fernando.rugge@pleaseup.com>
2020-02-29 13:09:27 +0100

    updated mail and env with dispersal duration

--- rest/resources/views/emails/contracts/open-dispute/creator.blade.php
+++ rest/resources/views/emails/contracts/open-dispute/creator.blade.php
@@ -2,7 +2,7 @@
 # Dispute Opened
 
 <p>Hey {{$creator}},<br>
-you <strong>have successfully rejected your counterparty's friendly resolution</strong> for the contract {{$contract->address}}.<br>The contract is now in a dispute state and it will wait for 24 hours for you counterparty's proposal. If the proposal does not arrive within the timeframe the smart legal contract will automatically propose the maximum for your counterparty. We will notify you of any udpate and you also can check the dispute's status by accessing the detail page directly:<br><a href="{{$url}}" class="button button-inline button-jur" target="_blank">Go to contract</a>
+you <strong>have successfully rejected your counterparty's friendly resolution</strong> for the contract {{$contract->address}}.<br>The contract is now in a dispute state and it will wait for {{ env('CHAIN_DISPUTE_DISPERSAL_DURATION','24') }} hours for you counterparty's proposal. If the proposal does not arrive within the timeframe the smart legal contract will automatically propose the maximum for your counterparty. We will notify you of any udpate and you also can check the dispute's status by accessing the detail page directly:<br><a href="{{$url}}" class="button button-inline button-jur" target="_blank">Go to contract</a>
 </p>
 
 Best,<br>
