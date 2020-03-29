df82edebb65213f80d177f8808d0ce790ec81c8d
tyagishuchi1 <shuchi@jur.io>
2020-03-30 01:03:51 +0530

    updated: network specific migration

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,16 +1,23 @@
 const OathKeeper = artifacts.require('./OathKeeper.sol');
 const JURToken = artifacts.require('@openzeppelin/contracts/token//ERC20/ERC20.sol');
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
 
-    return deployer
-        .then(() => {
-            return deployer.deploy(JURToken);
-        })
-        .then(() => {
-            return deployer.deploy(
-                OathKeeper,
-                JURToken.address
-            );
-        });
+    if (network == "live") {
+        return deployer.deploy(
+            OathKeeper,
+            process.env.JUR_TOKEN_ADDRESS
+        );
+    } else {
+        return deployer
+            .then(() => {
+                return deployer.deploy(JURToken);
+            })
+            .then(() => {
+                return deployer.deploy(
+                    OathKeeper,
+                    JURToken.address
+                );
+            });
+      }
 };
