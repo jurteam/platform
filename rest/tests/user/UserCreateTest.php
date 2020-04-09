41b189234dbf11de57dfef65b27136c3410028ca
MTM Suhail <getmtm@gmail.com>
2020-04-09 14:25:40 +0530

    🏆 test: should_not_create_user_with_invalid_data

--- rest/tests/user/UserCreateTest.php
+++ rest/tests/user/UserCreateTest.php
@@ -132,4 +132,64 @@ class UserCreateTest extends TestCase
         $this->seeInDatabase('users', array_merge($data, $header));
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_create_user_with_invalid_data()
+    {
+        $header = ['wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A1'];
+
+        $data = [
+            'name' => 'Bob',
+            'email' => 'bob', // invalid email
+            'gender' => 'feale', // invalid gender (accept only male,femail and other)
+            'location' => 'New York',
+            'birth_date' => '1989-30-08', // invalid date
+            'category' => 'General',
+            'show_fullname' => 'yes', // invalid data type
+            'accepted_terms' => 'no', // invalid data type
+            'accepted_disclaimer' => 'no', // invalid data type
+        ];
+
+        $this->post("api/v1/user", $data, $header);
+
+        // validate status
+        $this->seeStatusCode(401);
+
+        // validate stucture of data
+        $this->seeJsonStructure(
+            [
+                'errors' =>
+                [
+                    'email',
+                    'gender',
+                    'birth_date',
+                    'show_fullname',
+                    'accepted_terms',
+                    'accepted_disclaimer',
+                ],
+            ]
+        );
+
+        // validate data
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'email' => 'Email not valid',
+                    'gender' => 'Gender not valid',
+                    'birth_date' => 'Birth Date is an invalid date',
+                    'show_fullname' => 'Show Fullname is invalid',
+                    'accepted_terms' => 'Accepted Terms is invalid',
+                    'accepted_disclaimer' => 'Accepted Disclaimer is invalid',
+                ],
+            ]
+        );
+
+        // validate data not present in database
+        $this->notSeeInDatabase('users', $header);
+    }
+
 }
