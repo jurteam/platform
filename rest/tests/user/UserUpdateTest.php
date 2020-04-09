7e07bfd9d971a3cc9c5080b54e19d46181730a9a
MTM Suhail <getmtm@gmail.com>
2020-04-09 16:17:27 +0530

    🏆 test: should_not_update_user_without_wallet

--- rest/tests/user/UserUpdateTest.php
+++ rest/tests/user/UserUpdateTest.php
@@ -6,4 +6,15 @@ class UserUpdateTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_update_user_without_wallet()
+    {
+        $this->put("api/v1/user", [])
+            ->seeStatusCode(401);
+    }
+
 }
