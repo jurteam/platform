45fef08a847b473b8e08dda445ecaf626d40ad7c
MTM Suhail <getmtm@gmail.com>
2020-04-09 16:19:09 +0530

    🏆 test: should_update_user_name_using_wallet

--- rest/tests/user/UserUpdateTest.php
+++ rest/tests/user/UserUpdateTest.php
@@ -17,4 +17,44 @@ class UserUpdateTest extends TestCase
             ->seeStatusCode(401);
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_update_user_name_using_wallet()
+    {
+        $header = ['wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7'];
+        $data = ['name' => 'John Nash'];
+
+        // create user
+        $this->post("api/v1/user", [], $header);
+
+        // validate status of create
+        $this->seeStatusCode(201);
+
+        // update existing user's name
+        $this->put("api/v1/user", $data, $header);
+
+        // validate status of update
+        $this->seeStatusCode(200);
+
+        // validate stucture of data
+        $this->seeJsonStructure(
+            [
+                'user' =>
+                [
+                    'id',
+                    'name',
+                    'wallet',
+                    'updated_at',
+                    'created_at',
+                ],
+            ]
+        );
+
+        // validate data present in database
+        $this->seeInDatabase('users', array_merge($data, $header));
+    }
+
 }
