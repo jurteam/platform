33056be0e8a83cae8a2eb6d426aadf9c4cb483d4
MTM Suhail <getmtm@gmail.com>
2020-04-09 17:45:09 +0530

    🏆 test: should_not_delete_user_without_wallet

--- rest/tests/user/UserDeleteTest.php
+++ rest/tests/user/UserDeleteTest.php
@@ -6,4 +6,15 @@ class UserDeleteTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_delete_user_without_wallet()
+    {
+        $this->delete("api/v1/user", [])
+            ->seeStatusCode(401);
+    }
+
 }
