a78222cb4a9c9e49df6ad4f52291e4805b338b03
MTM Suhail <getmtm@gmail.com>
2020-04-09 14:13:48 +0530

    🏆 test: should_not_create_user_without_wallet

--- rest/tests/user/UserCreateTest.php
+++ rest/tests/user/UserCreateTest.php
@@ -6,4 +6,15 @@ class UserCreateTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_create_user_without_wallet()
+    {
+        $this->post("api/v1/user", [])
+            ->seeStatusCode(401);
+    }
+
 }
