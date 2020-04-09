be0fe87fb5afaee1966162d61d6daa7506fa4ba8
MTM Suhail <getmtm@gmail.com>
2020-04-09 14:21:55 +0530

    🏆 test: should_create_user_with_valid_data

--- rest/tests/user/UserCreateTest.php
+++ rest/tests/user/UserCreateTest.php
@@ -79,4 +79,57 @@ class UserCreateTest extends TestCase
             ->seeStatusCode(422);
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_create_user_with_valid_data()
+    {
+        $header = ['wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616AA'];
+
+        $data = [
+            'name' => 'Alice',
+            'email' => 'alice007@gmail.com',
+            'gender' => 'female',
+            'location' => 'New York',
+            'birth_date' => '1989-10-08',
+            'category' => 'General',
+            'show_fullname' => 1,
+            'accepted_terms' => 1,
+            'accepted_disclaimer' => 1,
+        ];
+
+        $this->post("api/v1/user", $data, $header);
+
+        // validate status
+        $this->seeStatusCode(201);
+
+        // validate stucture of data
+        $this->seeJsonStructure(
+            [
+                'user' =>
+                [
+                    'id',
+                    'wallet',
+                    'name',
+                    'email',
+                    'gender',
+                    'location',
+                    'birth_date',
+                    'category',
+                    'show_fullname',
+                    'accepted_terms',
+                    'accepted_disclaimer',
+                    'updated_at',
+                    'created_at',
+                ],
+            ]
+
+        );
+
+        // validate data present in database
+        $this->seeInDatabase('users', array_merge($data, $header));
+    }
+
 }
