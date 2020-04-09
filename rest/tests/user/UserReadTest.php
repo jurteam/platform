302d6c25649617e5eef8ef2a497d2981bd935d0e
MTM Suhail <getmtm@gmail.com>
2020-04-09 19:15:18 +0530

    🏆 test: should_not_read_user_without_wallet

--- rest/tests/user/UserReadTest.php
+++ rest/tests/user/UserReadTest.php
@@ -6,4 +6,30 @@ class UserReadTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_read_user_without_wallet()
+    {
+        $this->get("api/v1/user", []);
+
+        // validate status
+        $this->seeStatusCode(401);
+
+        // validate stucture of data
+        $this->seeJsonStructure(['errors']);
+
+        // validate response
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'wallet' => ['The wallet is missing.'],
+                ],
+            ]
+        );
+    }
+
 }
