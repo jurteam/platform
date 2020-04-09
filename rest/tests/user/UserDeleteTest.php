385157efdcf5309bee0932ba04656a5a21ba79ac
MTM Suhail <getmtm@gmail.com>
2020-04-09 17:48:07 +0530

    🏆 test: should_delete_user_name_using_wallet

--- rest/tests/user/UserDeleteTest.php
+++ rest/tests/user/UserDeleteTest.php
@@ -17,4 +17,46 @@ class UserDeleteTest extends TestCase
             ->seeStatusCode(401);
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_delete_user_name_using_wallet()
+    {
+        $header = ['wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7'];
+
+        // create user
+        $this->post("api/v1/user", [], $header);
+
+        // validate status of create
+        $this->seeStatusCode(201);
+
+        // validate data present in database
+        $this->seeInDatabase('users', $header);
+
+        // update existing user's name
+        $this->delete("api/v1/user", [], $header);
+
+        // validate status of update
+        $this->seeStatusCode(200);
+
+        // validate stucture of data
+        $this->seeJsonStructure(
+            [
+                'status',
+            ]
+        );
+
+        // validate data
+        $this->seeJson(
+            [
+                'status' => 'deleted',
+            ]
+        );
+
+        // validate data not present in database
+        $this->notseeInDatabase('users', $header);
+    }
+
 }
