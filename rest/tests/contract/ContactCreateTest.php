680d970ed4027e4f8815fbcb0271a12adf0ef5c4
MTM Suhail <getmtm@gmail.com>
2020-04-20 17:52:29 +0530

    🏆 test: should_not_create_contract_without_registred_user

--- rest/tests/contract/ContactCreateTest.php
+++ rest/tests/contract/ContactCreateTest.php
@@ -6,4 +6,36 @@ class ContractCreateTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_create_contract_without_registred_user()
+    {
+        $this->post("api/v1/contracts",
+            [
+                'part_a_wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+                'part_b_wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A1',
+            ],
+            ['wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A6']// non existing wallet
+        );
+
+        // validate status
+        $this->seeStatusCode(422);
+
+        // validate stucture of data
+        $this->seeJsonStructure(['errors']);
+
+        // validate data
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'user_id' => ['The User Id is not valid'],
+                ],
+            ]
+        );
+    }
+
 }
