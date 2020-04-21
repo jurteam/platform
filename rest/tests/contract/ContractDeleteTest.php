1f24d132a40728c77159fc0135955c459d348450
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:18:41 +0530

    🏆 test: should_delete_contract_using_wallet_and_contract_id

--- rest/tests/contract/ContractDeleteTest.php
+++ rest/tests/contract/ContractDeleteTest.php
@@ -34,4 +34,44 @@ class ContractDeleteTest extends TestCase
         ]);
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_delete_contract_using_wallet_and_contract_id()
+    {
+        $user = factory(App\Models\User::class)->create();
+        $contract = factory(App\Models\Contract::class)->create([
+            'user_id' => $user->id,
+            'wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'part_a_wallet' => $user->wallet,
+        ]);
+
+        // validate data present in database
+        $this->seeInDatabase('contracts', ['id' => $contract->id]);
+
+        // get encoded id
+        $id = encodeId($contract->id);
+
+        // try to delete
+        $this->delete("api/v1/contracts/{$id}", [], ['wallet' => $user->wallet]);
+
+        // validate status
+        $this->seeStatusCode(200);
+
+        // validate stucture of data
+        $this->seeJsonStructure(['id']);
+
+        // validate data
+        $this->seeJson(
+            [
+                'id' => $id,
+            ]
+        );
+
+        // validate data present in database
+        $this->notSeeInDatabase('contracts', ['id' => $contract->id]);
+    }
+
 }
