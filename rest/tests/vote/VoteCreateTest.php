90dbe8543b110ca7099d569168f4d1ed3f5a341f
MTM Suhail <getmtm@gmail.com>
2020-04-21 19:25:11 +0530

    🏆 test: should_create_contract_with_valid_data

--- rest/tests/vote/VoteCreateTest.php
+++ rest/tests/vote/VoteCreateTest.php
@@ -108,4 +108,47 @@ class VoteCreateTest extends TestCase
         $this->seeStatusCode(422);
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_create_contract_with_valid_data()
+    {
+        $wallet = '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7';
+
+        // create user
+        $user = factory(App\Models\User::class)->create([
+            'wallet' => $wallet,
+        ]);
+
+        // create a contract
+        $contract = factory(App\Models\Contract::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        // get encoded id
+        $id = encodeId($contract->id);
+
+        // create dispute
+        $this->post("api/v1/contracts/disputes/{$id}", ['code' => 35], ['wallet' => $wallet]);
+
+        // validate status
+        $this->seeStatusCode(200);
+
+        $data = [
+            'contract_id' => $id,
+            'amount' => 10,
+            'oracle_wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'wallet_part' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'message' => 'hello',
+            'hash' => 123,
+        ];
+
+        // create vote
+        $this->post("api/v1/votes", $data, ['wallet' => $wallet]);
+
+        // validate status
+        $this->seeStatusCode(200);
+    }
 }
