668ad99796b659aa310e8ae4d058a664ab5b8c3a
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:18:21 +0530

    🏆 test: should_not_delete_contract_without_wallet

--- rest/tests/contract/ContractDeleteTest.php
+++ rest/tests/contract/ContractDeleteTest.php
@@ -6,4 +6,32 @@ class ContractDeleteTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_delete_contract_without_wallet()
+    {
+        $user = factory(App\Models\User::class)->create();
+        $contract = factory(App\Models\Contract::class)->create([
+            'user_id' => $user->id,
+            'wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'part_a_wallet' => $user->wallet,
+        ]);
+
+        // get encoded id
+        $id = encodeId($contract->id);
+
+        // try to delete
+        $this->delete("api/v1/contracts/{$id}", []);
+
+        // validate status
+        $this->seeStatusCode(401);
+
+        $this->seeInDatabase('contracts', [
+            'id' => $contract->id,
+        ]);
+    }
+
 }
