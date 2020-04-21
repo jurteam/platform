e5038000947fc2a2df3fdefb224da2b3b2d1da49
Ashish Gaikwad <ashish@jur.io>
2020-04-21 19:28:34 +0530

    doc(oathkeeper): adds params per component in sample stories

--- src/components/sections/Profile/OathKeeper.stories-wip.js
+++ src/components/sections/Profile/OathKeeper.stories-wip.js
@@ -3,8 +3,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { withInfo } from "@storybook/addon-info";
 
-// TODO: put every new component in cost C => null
-
 // Commons Existing
 import {
   Avatar,
@@ -35,59 +33,70 @@ ChartCard.Footer = () => null;
 ChartCard.LineChart = () => null;
 ChartCard.DonutsChart = () => null;
 const AmountRangeFilter = () => null;
-const Section = () => null;
+const Box = () => null;
 
 // App Specific
 
-const WalletCard = () => (
-  <HeaderCard title="Wallet">
+const WalletCard = ({ address }) => (
+  <HeaderCard title="Wallet" info="lorem ipsum">
     <HeaderCard.Hero>
-      <Avatar seed={wallet.address} />
+      <Avatar seed={address} />
     </HeaderCard.Hero>
     <HeaderCard.Body>
-      <Text size="small">{wallet.address}</Text>
+      <Text size="small">{address}</Text>
     </HeaderCard.Body>
   </HeaderCard>
 );
 
-const RankCard = () => (
-  <HeaderCard title="Oath Keeper Rank">
+const RankCard = ({ rank }) => (
+  <HeaderCard title="Oath Keeper Rank" info="lorem another ipsum">
     <HeaderCard.Hero>
-      <Rank rank="2" />
+      <Rank rank={rank} />
     </HeaderCard.Hero>
     <HeaderCard.Body>
-      <Text size="small">Check Oathkeeping Ranking</Text>
+      <Link to="ranking page">
+        <Text size="small">Check Oathkeeping Ranking</Text>
+      </Link>
     </HeaderCard.Body>
   </HeaderCard>
 );
 
-const BalanceCard = () => (
-  <HeaderCard title="Oath Keeper Balance">
+const BalanceCard = ({ amount, onViewDetail, isOpen }) => (
+  <HeaderCard title="Oath Keeper Balance" info="lorem lorem">
     <HeaderCard.Hero>
-      <Amount value="123" />
+      <Amount value={amount} />
     </HeaderCard.Hero>
     <HeaderCard.Body>
-      <ViewDetailsAction />
+      <ViewDetailsAction isOpen={isOpen} onClick={onViewDetail} />
     </HeaderCard.Body>
   </HeaderCard>
 );
 
-const WithdrawOathButton = () => <Button>Withdraw</Button>;
+// On Click it will call releaseOath(oathId) in smart contract
+const WithdrawOathButton = ({ oathId }) => <Button>Withdraw</Button>;
 
 // It will render last cell in each row of OathsTimelineView
-const StatusAction = status =>
-  status == "unlocked" ? <WithdrawOathButton /> : <Text>{status}</Text>;
+const StatusAction = ({ status, oathId }) =>
+  status.toLowerCase() == "unlocked" ? (
+    <WithdrawOathButton oathId={oathId} />
+  ) : (
+    <Text>{status}</Text>
+  );
 
 const OathsTimelineView = oaths => "OathsTimelineView"; // TODO: write expressive version
 
+// The following components are always ment to be used together
+// So could be exported through a single object.
+// TakeOath.AmountInput, TakeOath.LockupSlider, etc.
 const TakeOathAmountInput = () => null;
 const TakeOathLockupSlider = () => null;
 const TakeOathCancelButton = () => null;
 const TakeOathSubmitButton = () => null;
 
-const TakeOathModal = () => (
-  <Modal>
-    <ModalHeader>New Oath</ModalHeader>
+// Will use Rect Context to manage form input
+const TakeOathModal = ({ isOpen, onRequestClose }) => (
+  <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <ModalHeader title="New Oath" />
     <ModalBody>
       <Text>Your commitment to the Jur ecosystem ...</Text>
       <Row>
@@ -103,148 +112,207 @@ const TakeOathModal = () => (
   </Modal>
 );
 
-const DurationSelector = () => null; // NOTE: name suggestions: RadioSelector, EnumFilter
+const EnumFilter = () => null; // NOTE: name suggestions: RadioSelector, DurationSelector
 
-const AverageAmountCard = () => (
+const AverageAmountCard = ({
+  value,
+  delta,
+  graph,
+  selectedEnum = "Last Month",
+  onEnumFilterChange
+}) => (
   <ChartCard>
     <ChartCard.Title>Average Amount Staked</ChartCard.Title>
     <Row>
       <ChartCard.Metric>
-        <Amount value={69.32} />
-        <Delta value={-2.1} />
+        <Amount value={value} />
+        <Delta value={delta} />
       </ChartCard.Metric>
-      <ChartCard.LineChart data={some2dArray} />
+      <ChartCard.LineChart data={graph} />
     </Row>
     <ChartCard.Footer>
-      <DurationSelector />
+      <EnumFilter selected={selectedEnum} onChange={onEnumFilterChange} />
     </ChartCard.Footer>
   </ChartCard>
 );
 
-const AmountStakedCard = () => (
+const AmountStakedCard = ({
+  value,
+  graph,
+  selectedEnum = "Last Month",
+  onEnumFilterChange
+}) => (
   <ChartCard>
     <ChartCard.Title>Amount Staked By Oath keeper</ChartCard.Title>
     <Row>
       <ChartCard.Metric>
-        <Amount value={100000.32} />
+        <Amount value={value} />
       </ChartCard.Metric>
-      <ChartCard.DonutsChart data={some2dArray} />
+      <ChartCard.DonutsChart data={graph} />
     </Row>
     <ChartCard.Footer>
-      <DurationSelector />
+      <EnumFilter selected={selectedEnum} onChange={onEnumFilterChange} />
     </ChartCard.Footer>
   </ChartCard>
 );
+
+const ActiveAmountCard = () => null; // same as AverageAmountCard
+const ActiveOathKeepersCard = () => null; // same as AverageAmountCard
 
 const RankStatusFilter = () => <Dropdown />;
 const RankAmountRangeFilter = () => <AmountRangeFilter />;
 const RankDateRangeFilter = () => <DateRangeFilter />;
 const RankSearchBox = () => <SearchBox />;
 
-const RankingFilters = () => (
+// It will manage individual filter changes and call onChange accordingly
+// For example, onStatusChange -> direct call, onSearchChange -> debounced call, etc
+const RankingFilters = ({ onChange }) => (
   <Row>
-    <RankStatusFilter />
+    <RankStatusFilter onChange={onStatusChange} selected={state.status} />
     <Expand />
-    <RankAmountRangeFilter />
-    <RankDateRangeFilter />
-    <RankSearchBox />
+    <RankAmountRangeFilter
+      onChange={onAmountRangeChange}
+      min={state.min}
+      max={state.max}
+    />
+    <RankDateRangeFilter
+      onChange={onDateRangeChange}
+      start={state.start}
+      end={state.end}
+    />
+    <RankSearchBox onChange={onSearchChange} query={state.query} />
   </Row>
 );
 
-const RankingTableHeaderRow = () => null;
-const RankingTableRow = () => null;
+const RankingTableHeaderRow = ({ onSort }) => null;
+const RankingTableRow = ({ rank, address, amount, oathsCount }) => null;
 
-const RankingTable = () => (
+const RankingTable = ({ rows, isLoading, onSortChange }) => (
   <Table>
     <TableHead>
-      <RankingTableHeaderRow />
+      <RankingTableHeaderRow onSort={onSortChange} />
     </TableHead>
     <TableBody>
-      {rows.map(r => (
-        <RankingTableRow key={r.address} {...r} />
-      ))}
+      {isLoading ? (
+        <Spin />
+      ) : (
+        rows.map(r => <RankingTableRow key={r.address} {...r} />)
+      )}
     </TableBody>
   </Table>
 );
 
-const RankingTableStats = () => null;
-const RankingTablePagination = () => null;
+const RankingTableStats = ({ activePage, total, perPage = 10 }) =>
+  `${activePage}-${activePage + perPage} of ${total}`;
 
-const RankingFooter = () => (
+const RankingTablePagination = ({
+  active,
+  total,
+  onPageChange,
+  perPage = 10
+}) => (
+  <Pagination
+    activePage={active}
+    itemsCountPerPage={perPage}
+    totalItemsCount={total}
+    handlePageChange={onPageChange}
+  />
+);
+
+const RankingFooter = ({ active, total, onPageChange }) => (
   <Row>
-    <RankingTableStats />
+    <RankingTableStats active={active} total={total} />
     <Expand />
-    <RankingTablePagination />
+    <RankingTablePagination
+      active={active}
+      total={total}
+      onPageChange={onPageChange}
+    />
   </Row>
 );
 
-const ActiveOathsTable = () => null;
-const TakeOathButton = () => null;
-const TakeOathButton = () => null;
-const TakeOathButton = () => null;
+const ActiveOathsTable = ({ isHidden = true, oaths }) => null;
+
+// button manages state internally. It has modal also 😎
 const TakeOathButton = () => null;
 
-// Sections
+// Boxs
 
-const HeaderSection = () => (
-  <Section type="header">
+// manages state for view details action
+const MyOathsHeaderBox = ({ oaths }) => (
+  <Box type="header">
     <Row>
       <WalletCard />
       <RankCard />
       <BalanceCard />
     </Row>
-    <ActiveOathsTable hide={true} />
-  </Section>
+    <ActiveOathsTable isHidden={true} oaths={oaths} />
+  </Box>
 );
 
-const TakeOathSection = () => (
-  <Section type="main">
+const TakeOathBox = () => (
+  <Box type="main">
     <Text>Oath Keepers are Jur supporters ...</Text>
     <TakeOathButton />
-  </Section>
+  </Box>
 );
 
-const YourOathsSection = () => (
-  <Section title="Your Oaths">
-    {oaths ? (
+const YourOathsBox = ({ oaths }) => (
+  <Box title="Your Oaths">
+    {oaths.length ? (
       <OathsTimelineView oaths={oaths} />
     ) : (
       <Message text="You have not ..." />
     )}
     <TakeOathButton type="compact" />
-  </Section>
+  </Box>
 );
 
-const FooterSection = () => (
-  <Section type="footer" title="Do you want to learn...">
+const FooterBox = () => (
+  <Box type="footer" title="Do you want to learn...">
     <Text>Check out article ...</Text>
-  </Section>
+  </Box>
 );
 
-const RankingTableSection = () => (
-  <Section>
+// fetches data on load & handles filter change
+const RankingTableBox = () => (
+  <Box>
     <RankingFilters />
     <RankingTable />
     <RankingFooter />
+  </Box>
+);
+
+// loads initial stats & handles enum filter change per card
+const RankingHeaderBox = () => (
+  <Box>
+    <Row>
+      <ActiveAmountCard onEnumFilterChange={onActiveAmountEmumFilterChange} />
+      <AmountStakedCard onEnumFilterChange={onAmountStakedEmumFilterChange} />
+      <AverageAmountCard onEnumFilterChange={onAverageAmountEmumFilterChange} />
+      <ActiveOathKeepersCard
+        onEnumFilterChange={onActiveOathKeeperEmumFilterChange}
+      />
+    </Row>
+  </Box>
+);
+
+// Sections
+
+const MyOathsSection = () => (
+  <Section>
+    <MyOathsHeaderBox oaths={activeOaths(oaths)} />
+    <TakeOathBox />
+    <YourOathsBox oaths={state.oaths} />
+    <FooterBox />
   </Section>
 );
 
-// Screens
-
-const MyOathsScreen = () => (
-  <Screen>
-    <HeaderSection />
-    <TakeOathSection />
-    <YourOathsSection />
-    <FooterSection />
-  </Screen>
-);
-
-const OathRankingScreen = () => (
-  <Screen>
-    <HeaderSection />
-    <RankingTableSection />
-  </Screen>
+const OathRankingSection = () => (
+  <Section>
+    <RankingHeaderBox />
+    <RankingTableBox />
+  </Section>
 );
 
 // Stories
@@ -258,12 +326,11 @@ storiesOf("OathKeeper", module)
     }
   })
   .add("Navigation - Top", () => <MenuItem />)
-  .add("Navigation - In Profile", () => <MenuItem type="compact" />)
-  .add("Screen - MyOaths", () => <MyOathsScreen />)
+  .add("Section - MyOaths", () => <MyOathsSection />)
   .add("MyOaths - HeaderCard - Balance", () => <BalanceCard />)
   .add("MyOaths - HeaderCard - Rank", () => <RankCard />)
-  .add("MyOaths - Header", () => <HeaderSection />)
-  .add("MyOaths - TakeOath", () => <TakeOathSection />)
+  .add("MyOaths - Header", () => <HeaderBox />)
+  .add("MyOaths - TakeOath", () => <TakeOathBox />)
   .add("MyOaths - TakeOathModal", () => <TakeOathModal />)
-  .add("MyOaths - YourOaths", () => <YourOathsSection />)
-  .add("Screen - Ranking", () => <OathRankingScreen />);
+  .add("MyOaths - YourOaths", () => <YourOathsBox />)
+  .add("Section - Ranking", () => <OathRankingSection />);
