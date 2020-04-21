bb9d61e690eaf2a0f98fd76ac9d47bbbe9d7c477
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:16:10 +0530

    🏆 test: should_upload_a_file_with_an_existing_contract

--- rest/tests/Attachment/AttachmentCreateTest.php
+++ rest/tests/Attachment/AttachmentCreateTest.php
@@ -8,4 +8,48 @@ class AttachmentCreateTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_upload_a_file_with_an_existing_contract()
+    {
+        $wallet = '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7';
+
+        // create user
+        $user = factory(App\Models\User::class)->create([
+            'wallet' => $wallet,
+        ]);
+
+        // generate fake image
+        Storage::fake('attachments');
+
+        $attachments[] = UploadedFile::fake()->image('attachment.jpg');
+
+        // create a contract
+        $contract = factory(App\Models\Contract::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        // get encoded id
+        $id = encodeId($contract->id);
+
+        // update contract with attachment
+        $response = $this->put("api/v1/contracts/{$id}", [
+            'attachments' => $attachments,
+            'kpi' => 'test',
+        ], [
+            'wallet' => $wallet,
+        ]);
+
+        // validate status
+        $this->seeStatusCode(200);
+
+        // check attachment available in database
+        $this->seeInDatabase('media', [
+            'model_type' => \App\Models\Contract::class,
+        ]);
+    }
+
 }
