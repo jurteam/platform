f6c40f5e8a848ac5c5a5ba40fba3084183ba458f
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:15:29 +0530

    Create AttachmentCreateTest.php

---
+++ rest/tests/Attachment/AttachmentCreateTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Laravel\Lumen\Testing\DatabaseTransactions;
+
+class AttachmentCreateTest extends TestCase
+{
+    use DatabaseTransactions;
+
+}
