4e0d3e1474e9d824b91a06f74ae9bbe0bf86de7c
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:29:12 +0530

    🏆 test: should_not_read_dispute_without_wallet

--- rest/tests/contract/dispute/DisputeReadAllTest.php
+++ rest/tests/contract/dispute/DisputeReadAllTest.php
@@ -6,4 +6,30 @@ class DisputeReadAllTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_read_dispute_without_wallet()
+    {
+        $this->get("api/v1/contracts/disputes/all", []);
+
+        // validate status
+        $this->seeStatusCode(401);
+
+        // validate stucture of data
+        $this->seeJsonStructure(['errors']);
+
+        // validate response
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'wallet' => ['The wallet is missing.'],
+                ],
+            ]
+        );
+    }
+
 }
