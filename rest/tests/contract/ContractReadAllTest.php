66b572ff143380ba94ce790c97a15c0c3436444e
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:20:30 +0530

    🏆 test: should_not_read_contract_without_wallet

--- rest/tests/contract/ContractReadAllTest.php
+++ rest/tests/contract/ContractReadAllTest.php
@@ -6,4 +6,30 @@ class ContractReadAllTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_read_contract_without_wallet()
+    {
+        $this->get("api/v1/contracts", []);
+
+        // validate status
+        $this->seeStatusCode(401);
+
+        // validate stucture of data
+        $this->seeJsonStructure(['errors']);
+
+        // validate response
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'wallet' => ['The wallet is missing.'],
+                ],
+            ]
+        );
+    }
+
 }
