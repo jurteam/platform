799b150b5938aead1e351eb610236a6864e72a33
MTM Suhail <getmtm@gmail.com>
2020-04-21 19:22:50 +0530

    🏆 test: should_not_read_vote_with_invalid_wallet

--- rest/tests/vote/VoteReadAllTest.php
+++ rest/tests/vote/VoteReadAllTest.php
@@ -69,4 +69,65 @@ class VoteReadAllTest extends TestCase
         );
     }
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_read_vote_with_invalid_wallet()
+    {
+
+        $wallet = '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7';
+
+        // create user
+        $user = factory(App\Models\User::class)->create([
+            'wallet' => $wallet,
+        ]);
+
+        // create a contract
+        $contract = factory(App\Models\Contract::class)->create([
+            'user_id' => $user->id,
+        ]);
+
+        // get encoded id
+        $id = encodeId($contract->id);
+
+        // create dispute
+        $this->post("api/v1/contracts/disputes/{$id}", ['code' => 35], ['wallet' => $wallet]);
+
+        // validate status
+        $this->seeStatusCode(200);
+
+        $data = [
+            'contract_id' => $id,
+            'amount' => 10,
+            'oracle_wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'wallet_part' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'message' => 'hello',
+            'hash' => 123,
+        ];
+
+        // create vote
+        $this->post("api/v1/votes", $data, ['wallet' => $wallet]);
+
+        // validate status
+        $this->seeStatusCode(200);
+
+        // create vote
+        $this->get("api/v1/votes/{$id}", ['wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A']); // invalid wallet address (missing last charecter)
+
+        // validate status
+        $this->seeStatusCode(422);
+
+        // validate error details
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'wallet' => ['The wallet is not valid.'],
+                ],
+            ]
+        );
+    }
+
 }
