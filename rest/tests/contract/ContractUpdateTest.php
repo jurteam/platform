22b19ab301fd16800bc5cc2a0883dd530263a6f9
MTM Suhail <getmtm@gmail.com>
2020-04-21 12:24:59 +0530

    🏆 test: should_not_update_contract_without_wallet

--- rest/tests/contract/ContractUpdateTest.php
+++ rest/tests/contract/ContractUpdateTest.php
@@ -6,4 +6,43 @@ class ContractUpdateTest extends TestCase
 {
     use DatabaseTransactions;
 
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function should_not_update_contract_without_wallet()
+    {
+        $user = factory(App\Models\User::class)->create();
+        $contract = factory(App\Models\Contract::class)->create([
+            'user_id' => $user->id,
+            'wallet' => '0xdab6AbeF495D2eeE6E4C40174c3b52D3Bc9616A7',
+            'part_a_wallet' => $user->wallet,
+        ]);
+
+        // validate data present in database
+        $this->seeInDatabase('contracts', ['id' => $contract->id]);
+
+        // get encoded id
+        $id = encodeId($contract->id);
+
+        $this->put("api/v1/contracts/{$id}", []);
+
+        // validate status
+        $this->seeStatusCode(401);
+
+        // validate stucture of data
+        $this->seeJsonStructure(['errors']);
+
+        // validate data
+        $this->seeJson(
+            [
+                'errors' =>
+                [
+                    'wallet' => ['The wallet is missing.'],
+                ],
+            ]
+        );
+    }
+
 }
