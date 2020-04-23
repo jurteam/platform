7a9300ef4a5d2c159692ad5c4f01a78a71d16f54
MTM Suhail <getmtm@gmail.com>
2020-04-23 14:36:29 +0530

    created: getCards function
    GET analytical details of all cards for a period.

--- rest/app/Http/Controllers/OathKeeperController.php
+++ rest/app/Http/Controllers/OathKeeperController.php
@@ -6,4 +6,15 @@ use Illuminate\Http\Request;
 
 class OathKeeperController extends Controller
 {
+    /**
+     * GET analytical details of all cards for a period.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function getCards(Request $request)
+    {
+        return response()->json(['status' => 'success']);
+    }
+
 }
