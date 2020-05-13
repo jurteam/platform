38e8717714046ef08833b74c45ec5f97b00432ae
tyagishuchi1 <shuchi@jur.io>
2020-05-13 17:10:47 +0530

    updated: truffle config includes network based settings

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,20 @@
 module.exports = {
   networks: {
-    development: {
+    "development": {
       host: "localhost",
-      port: 7545,
+      port: 8545,
+      gas: 6721975,
+      network_id: "*" // Match any network id
+    },
+    "testnet": {
+      host: "localhost",
+      port: 8545,
+      gas: 6721975,
+      network_id: "*" // Match any network id
+    },
+    "mainnet": {
+      host: "localhost",
+      port: 8545,
       gas: 6721975,
       network_id: "*" // Match any network id
     }
