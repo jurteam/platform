125aabb81aabfa9586d56b05261ca1b41c8ecae0
tyagishuchi1 <shuchi@jur.io>
2020-05-13 17:09:02 +0530

    updated: deploy script

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,23 +1,21 @@
 const OathKeeper = artifacts.require('./OathKeeper.sol');
-const JURToken = artifacts.require('@openzeppelin/contracts/token//ERC20/ERC20.sol');
+const JURToken = artifacts.require('@openzeppelin/contracts/token/ERC20/ERC20.sol');
 
-module.exports = function(deployer, network) {
+const Web3 = require('web3');
+web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
-    if (network == "live") {
-        return deployer.deploy(
-            OathKeeper,
-            process.env.JUR_TOKEN_ADDRESS
-        );
-    } else {
-        return deployer
-            .then(() => {
-                return deployer.deploy(JURToken);
-            })
-            .then(() => {
-                return deployer.deploy(
-                    OathKeeper,
-                    JURToken.address
-                );
+module.exports = function (deployer, network, accounts) {
+    var JUR_TOKEN = require('../config.json')[network.toString()].JUR_TOKEN;
+    console.log("JUR Token address:", JUR_TOKEN);
+    
+    if (network == "development") {
+        return deployer.deploy(JURToken).then(() => {
+            return JURToken.deployed().then((jurToken) => {
+              return deployer.deploy(OathKeeper, JURToken.address);
             });
+        });
+      } else {
+        return deployer.deploy(OathKeeper, JUR_TOKEN);
       }
-};
+  
+  }
