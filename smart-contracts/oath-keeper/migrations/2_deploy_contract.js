5565b9db138c79e4a38dcf5e3b386116c6ab5601
tyagishuchi1 <shuchi@jur.io>
2020-05-14 16:40:37 +0530

    feat(SmartContracts):oathkeeper migration

--- smart-contracts/oath-keeper/migrations/2_deploy_contract.js
+++ smart-contracts/oath-keeper/migrations/2_deploy_contract.js
@@ -1,21 +1,13 @@
 const OathKeeper = artifacts.require('./OathKeeper.sol');
-const JURToken = artifacts.require('@openzeppelin/contracts/token/ERC20/ERC20.sol');
+const deployHelper = require('../../helper.js');
 
 const Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 module.exports = function (deployer, network, accounts) {
-    var JUR_TOKEN = require('../config.json')[network.toString()].JUR_TOKEN;
+    var JUR_TOKEN = deployHelper.getJURToken(network);
     console.log("JUR Token address:", JUR_TOKEN);
-    
-    if (network == "development") {
-        return deployer.deploy(JURToken).then(() => {
-            return JURToken.deployed().then((jurToken) => {
-              return deployer.deploy(OathKeeper, JURToken.address);
-            });
-        });
-      } else {
-        return deployer.deploy(OathKeeper, JUR_TOKEN);
-      }
+
+    return deployer.deploy(OathKeeper, JUR_TOKEN);
   
   }
