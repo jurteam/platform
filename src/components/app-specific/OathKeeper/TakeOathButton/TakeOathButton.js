afd00774a25a0584003b6af1ecc057c7a752ce42
Ashish Gaikwad <ashish@jur.io>
2020-05-20 16:43:57 +0530

    feat(OathKeeper): shows take oath errors

--- src/components/app-specific/OathKeeper/TakeOathButton/TakeOathButton.js
+++ src/components/app-specific/OathKeeper/TakeOathButton/TakeOathButton.js
@@ -11,13 +11,13 @@ const color = type => (type === "compact" ? "outline" : "gradient");
 const label = type => (type === "compact" ? "New Oath" : "Oath Now");
 const size = type => (type === "compact" ? "small" : undefined);
 
-const TakeOathButton = ({ isOpen, open, close, type }) => {
+const TakeOathButton = ({ isOpen, open, close, type, message }) => {
   return (
     <>
       <Button color={color(type)} size={size(type)} onClick={open}>
         {label(type)}
       </Button>
-      <TakeOathModal isOpen={isOpen} onRequestClose={close} />
+      <TakeOathModal isOpen={isOpen} onRequestClose={close} message={message} />
     </>
   );
 };
@@ -26,7 +26,10 @@ TakeOathButton.defaultProps = {
   type: "normal"
 };
 
-const mapStateToProps = state => ({ isOpen: state.oathKeeper.isModalOpen });
+const mapStateToProps = state => ({
+  isOpen: state.oathKeeper.isModalOpen,
+  message: state.oathKeeper.newOathMessage
+});
 
 const open = () => ({ type: OATH_KEEPER_OPEN });
 const close = () => ({ type: OATH_KEEPER_CLOSE });
--- src/sagas/OathKeeper.js
+++ src/sagas/OathKeeper.js
@@ -16,7 +16,8 @@ import {
   OATH_KEEPER_WITHDREW_OATH,
   OATH_KEEPER_FETCH_OATHS_OF,
   OATH_KEEPER_UPDATE_OATHS_OF,
-  HOUND_START_SMELLING
+  HOUND_START_SMELLING,
+  OATH_KEEPER_REJECT_OATH
 } from "../reducers/types";
 
 function* fetchMyRank() {
@@ -36,20 +37,33 @@ function* takeAnOath() {
   const myOaths = yield select(getMyOaths);
   const oathIndex = myOaths.length;
 
-  const prey = yield new connexOathKeeper().takeAnOath(
-    address,
-    amount,
-    lockInPeriod
-  );
-  yield put({ type: OATH_KEEPER_TOOK_OATH });
+  try {
+    const prey = yield new connexOathKeeper().takeAnOath(
+      address,
+      amount,
+      lockInPeriod
+    );
 
-  prey.onFound = () =>
-    global.store.dispatch({
-      type: OATH_KEEPER_FETCH_MY_OATHS,
-      payload: oathIndex
+    yield put({ type: OATH_KEEPER_TOOK_OATH });
+
+    prey.onFound = () =>
+      global.store.dispatch({
+        type: OATH_KEEPER_FETCH_MY_OATHS,
+        payload: oathIndex
+      });
+
+    yield put({ type: HOUND_START_SMELLING, payload: prey });
+  } catch (e) {
+    console.error("Failed to take an oath", e);
+    yield put({
+      type: OATH_KEEPER_REJECT_OATH,
+      error: e,
+      payload: {
+        message: e.message,
+        oathIndex
+      }
     });
-
-  yield put({ type: HOUND_START_SMELLING, payload: prey });
+  }
 }
 
 function* withdrawAnOath(action) {
