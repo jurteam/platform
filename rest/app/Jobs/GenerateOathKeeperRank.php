29e58a9dd78f47dac173555af5411496174d3f0d
MTM Suhail <getmtm@gmail.com>
2020-05-27 22:45:45 +0530

    updated: implemented oath-keeper ranking algorithm

--- rest/app/Jobs/GenerateOathKeeperRank.php
+++ rest/app/Jobs/GenerateOathKeeperRank.php
@@ -2,17 +2,21 @@
 
 namespace App\Jobs;
 
+use \App\Models\Oath;
 use \App\Models\OathKeeper;
+use \Carbon\Carbon;
 
 class GenerateOathKeeperRank extends Job
 {
+    private $oathKeeper;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($oathKeeper)
     {
+        $this->oathKeeper = $oathKeeper;
     }
 
     /**
@@ -22,6 +26,40 @@ class GenerateOathKeeperRank extends Job
      */
     public function handle()
     {
+        $oaths = Oath::where(['oath_keeper_id' => $this->oathKeeper->id])->orderBy('oath_index', 'asc')->get();
+
+        $pastOaths = [];
+
+        $rankPoint = 0;
+
+        foreach ($oaths as $oath) {
+
+            // find the basic point
+            $basicPoint = $oath->amount * pow($oath->lock_in_period, config('rank.basicMultiplier'));
+
+            // find the fiat point
+            $fiatPoint = config('rank.fiatMultiplier') * (config('rank.fiatReference') / $oath->fiat_value) * $basicPoint;
+
+            $sumHistory = 0;
+            $currentTimeStamp = Carbon::createFromDate($oath->start_at)->timestamp;
+            foreach ($pastOaths as $past) {
+                $prevTimeStamp = Carbon::createFromDate($past->start_at)->timestamp;
+                $sumHistory = $sumHistory + (($basicPoint + $fiatPoint) * pow(abs($currentTimeStamp - $prevTimeStamp), config('rank.oldnessMultiplier')));
+            }
+
+            // find the history point
+            $historyPoint = config('rank.historyMultiplier') * $sumHistory;
+
+            $rankPoint = $rankPoint + ($basicPoint + $fiatPoint + $historyPoint);
+
+            // add oath to past oaths
+            array_push($pastOaths, $oath);
+        }
+
+        $this->oathKeeper->rank_point = $rankPoint;
+
+        $this->oathKeeper->save();
+
         $rank = 0;
 
         $oathKeepers = OathKeeper::orderBy('active_amount', 'desc')->get();
