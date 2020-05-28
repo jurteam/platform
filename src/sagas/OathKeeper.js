26962bfb1aab9513cd87181927375fd712d43654
Ashish Gaikwad <ashish@jur.io>
2020-05-28 13:34:27 +0530

    fix(OathKeeper): preserves failed oath's info
    Resolves: #120

--- src/sagas/OathKeeper.js
+++ src/sagas/OathKeeper.js
@@ -19,6 +19,7 @@ import {
   HOUND_START_SMELLING,
   OATH_KEEPER_REJECT_OATH
 } from "../reducers/types";
+import { oathState } from "../utils/helpers";
 
 function* fetchMyRank() {
   const { address } = yield select(getWallet);
@@ -99,7 +100,9 @@ function* fetchMyOaths(action) {
     );
 
     // if (myOaths.length + 1 > action.payload)
-    myOaths = myOaths.map(o => (o.oathIndex === action.payload ? oath : o));
+    myOaths = myOaths.map(o =>
+      o.oathIndex === action.payload ? preserveOathInfo(oath, o) : o
+    );
     // else myOaths = [oath, ...myOaths];
   } else {
     myOaths = yield new connexOathKeeper().fetchOathsOf(address);
@@ -135,6 +138,15 @@ function* fetchAnalytics(action) {
     payload: analytics,
     card: action.card
   });
+}
+
+function preserveOathInfo(oath, original) {
+  if (Number(oath.lockInPeriod) < 1) {
+    oath.lockInPeriod = original.lockInPeriod;
+    oath.amount = original.amount;
+    oath.customStatus = oathState.FAILED;
+  }
+  return oath;
 }
 
 export default function* oathKeeperSagas() {
