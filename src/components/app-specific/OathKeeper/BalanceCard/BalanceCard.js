e0258cd7b3f92a91b50438dd4a55a3d6005b642e
Ashish Gaikwad <ashish@jur.io>
2020-06-01 12:09:15 +0530

    style(OathKeeper): updates font size and colors
    In Balance card and Rank card, body's font size is set to 10px
    as per design. Color of View Detials change as per balance
    
    Resolves: #129

--- src/components/app-specific/OathKeeper/BalanceCard/BalanceCard.js
+++ src/components/app-specific/OathKeeper/BalanceCard/BalanceCard.js
@@ -5,13 +5,13 @@ import HeaderCard from "JurCommon/HeaderCard";
 import Amount from "JurCommon/Amount";
 import { AngleIcon } from "JurCommon/Icons/AngleIcon";
 
-const IconHandler = ({ isShown }) => (
+const IconHandler = ({ isShown, fill }) => (
   <div
     className={`jur-balance-card-icon jur-balance-card-icon__${
       isShown ? "down" : "up"
     }`}
   >
-    <AngleIcon />
+    <AngleIcon fill={fill} />
   </div>
 );
 
@@ -21,8 +21,17 @@ const BalanceCard = ({ balance, isShown, onViewDetails }) => (
       <Amount value={balance} />
     </HeaderCard.Hero>
     <HeaderCard.Body>
-      <a onClick={onViewDetails}>
-        View Details <IconHandler isShown={isShown} />
+      <a
+        onClick={onViewDetails}
+        className={`jur-balance-card-view-details jur-balance-card-view-details__${
+          balance > 0 ? "active" : "inactive"
+        }`}
+      >
+        View Details{" "}
+        <IconHandler
+          isShown={isShown}
+          fill={balance > 0 ? "#0077ff" : undefined}
+        />
       </a>
     </HeaderCard.Body>
   </HeaderCard>
