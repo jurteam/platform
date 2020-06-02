fa50b0a875ef207f67a98789e2cb61deb734086c
tyagishuchi1 <shuchi@jur.io>
2020-06-02 14:26:48 +0530

    refactor(polling-service): modules seperating web3 calls

--- polling-service/helpers/event-helper.js
+++ polling-service/helpers/event-helper.js
@@ -4,6 +4,39 @@ const getBlock = block => {
   return web3.eth.getBlock(block);
 };
 
+const getTransaction = async txHash => {
+  let tx = await web3.eth.getTransactionReceipt(txHash);
+  if (tx?.outputs.length > 0) {
+    return tx
+  }
+}
+const processTx = async tx => {
+    let eventsList = [];
+    tx?.outputs.forEach(out => {
+      out.events.forEach(event => {
+        let contract = findContractByAddress(
+          event.address
+        );
+        if (contract != null) {
+          let result = decodeEventData(
+            contract,
+            event.data,
+            event.topics
+          );
+          if (result) {
+            result.txHash = tx.address;
+            result.sender = tx.meta.txOrigin;
+            result.blockNumber = tx.blockNumber;
+            result.timestamp = tx.meta.blockTimestamp;
+            result.contractName = contract.identifier;
+            eventsList.push(result);
+          }
+        }
+      })
+    });
+    return eventsList;
+};
+
 const decodeEventData = (abi, data, topics) => {
   for (let i = 0; i < abi.events.length; i++) {
     if (web3.eth.abi.encodeEventSignature(abi.events[i]) === topics[0]) {
@@ -32,6 +65,6 @@ const findContractByAddress = address => {
 
 module.exports = {
   getBlock,
-  decodeEventData,
-  findContractByAddress
+  getTransaction,
+  processTx
 };
--- polling-service/src/event-processor.js
+++ polling-service/src/event-processor.js
@@ -1,37 +1,8 @@
 const eventHelper = require("../helpers/event-helper.js");
 
 const findEventInTransaction = async txHash => {
-  let res = await web3.eth.getTransactionReceipt(txHash);
-  if (res.outputs.length > 0) {
-    let eventsList = [];
-    for (let i = 0; i < res.outputs.length; i++) {
-      let event = res.outputs[i].events;
-      if (event && event.length > 0) {
-        for (let j = 0; j < event.length; j++) {
-          let contract = eventHelper.findContractByAddress(
-            res.outputs[i].events[j].address
-          );
-          if (contract != null) {
-            let result = eventHelper.decodeEventData(
-              contract,
-              res.outputs[i].events[j].data,
-              res.outputs[i].events[j].topics
-            );
-            if (result) {
-              result.txHash = txHash;
-              result.sender = res.meta.txOrigin;
-              result.blockNumber = res.blockNumber;
-              result.timestamp = res.meta.blockTimestamp;
-              result.contractName = contract.identifier;
-              eventsList.push(result);
-            }
-          }
-        }
-      } else return null;
-    }
-    // console.log("[polling-service-event-helper] Event List", eventsList)
-    return eventsList;
-  } else return null;
+  let tx = await eventHelper.getTransaction(txHash);
+  return await eventHelper.processTx(tx); 
 };
 
 const checkBlock = async transactions => {
