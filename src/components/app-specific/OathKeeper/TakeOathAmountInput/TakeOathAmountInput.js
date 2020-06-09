bc3ab57796ad5f19fb0ab1522b301dfee65f6bdc
Ashish Gaikwad <ashish@jur.io>
2020-06-09 18:42:02 +0530

    feat(OathKeeper): shows message to user if oath abount is more than balance
    Resolves: #167

--- src/components/app-specific/OathKeeper/TakeOathAmountInput/TakeOathAmountInput.js
+++ src/components/app-specific/OathKeeper/TakeOathAmountInput/TakeOathAmountInput.js
@@ -5,8 +5,9 @@ import Form from "JurCommon/Form";
 import Col from "JurCommon/Col";
 import { OATH_KEEPER_UPDATE_AMOUNT } from "../../../../reducers/types";
 import { MIN_TOKEN_AMOUNT } from "../../../../api/connex/OathKeeper";
+import { getWalletBalance, getNewOath } from "../../../../sagas/Selectors";
 
-const TakeOathAmountInput = ({ onChange }) => (
+const TakeOathAmountInput = ({ onChange, balance, value }) => (
   <Col>
     <Form.Label className="jur-take-oath__label">Token Amount</Form.Label>
     <Form.NumericInput
@@ -16,6 +17,9 @@ const TakeOathAmountInput = ({ onChange }) => (
       className="jur-oath-amount-input"
       max={Number.MAX_SAFE_INTEGER}
       min={MIN_TOKEN_AMOUNT}
+      errorMsg={
+        Number(balance) < Number(value) && "You do not have enough JUR balance"
+      }
     />
   </Col>
 );
@@ -25,6 +29,15 @@ const onChange = e => ({
   payload: e
 });
 
+const mapStateToProps = state => ({
+  balance: getWalletBalance(state),
+  value: getNewOath(state).amount
+});
+
 const mapDispatchToProps = { onChange };
 
-export default global.connection(TakeOathAmountInput, null, mapDispatchToProps);
+export default global.connection(
+  TakeOathAmountInput,
+  mapStateToProps,
+  mapDispatchToProps
+);
