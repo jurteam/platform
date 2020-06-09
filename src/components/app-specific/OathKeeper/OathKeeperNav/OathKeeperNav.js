d6302dc2a2a1aa63204bc30a84195c5767a37643
Ashish Gaikwad <ashish@jur.io>
2020-06-09 11:55:04 +0530

    style(OathKeeper): aligns top nav, bold when active
    Resolves: #158 and #157

--- src/components/app-specific/OathKeeper/OathKeeperNav/OathKeeperNav.js
+++ src/components/app-specific/OathKeeper/OathKeeperNav/OathKeeperNav.js
@@ -4,8 +4,13 @@ import { OathKeeperNavIcon } from "JurCommon/Icons";
 import { NavLink } from "react-router-dom";
 
 const OathKeeperNav = () => (
-  <NavLink to="/profile/my-oaths" className="jur-oath-keeper-nav">
-    <OathKeeperNavIcon /> Oath Keeper
+  <NavLink
+    to="/profile/my-oaths"
+    activeClassName="jur-oath-keeper-nav__active"
+    className="jur-oath-keeper-nav"
+  >
+    <OathKeeperNavIcon />{" "}
+    <span className="jur-oath-keeper-nav__label">Oath Keeper</span>
   </NavLink>
 );
 export default OathKeeperNav;
