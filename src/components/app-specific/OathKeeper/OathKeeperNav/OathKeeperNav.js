97f21062b1145f94c2a633602c064f1acf5b105d
Ashish Gaikwad <ashish@jur.io>
2020-06-10 11:41:44 +0530

    fix(OathKeeper): put oath keeper routes under oath-keeper
    Resolves: #157

--- src/components/app-specific/OathKeeper/OathKeeperNav/OathKeeperNav.js
+++ src/components/app-specific/OathKeeper/OathKeeperNav/OathKeeperNav.js
@@ -5,20 +5,13 @@ import { NavLink } from "react-router-dom";
 
 const OathKeeperNav = () => (
   <NavLink
-    to="/profile/my-oaths"
+    to="/oath-keeper/my-oaths"
     activeClassName="jur-oath-keeper-nav__active"
     className="jur-oath-keeper-nav"
-    isActive={isActive}
   >
     <OathKeeperNavIcon />{" "}
     <span className="jur-oath-keeper-nav__label">Oath Keeper</span>
   </NavLink>
 );
-
-function isActive(match, location) {
-  if (location.pathname === "/oath-keeper/oath-takers") return true;
-  if (match) return true;
-  return false;
-}
 
 export default OathKeeperNav;
