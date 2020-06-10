6b2bb93d50f2936111dd38ba932bec82203f9964
Ashish Gaikwad <ashish@jur.io>
2020-06-10 17:58:05 +0530

    style(OathKeeper): makes search icon grey and aligns
    Refs: #153

--- src/components/app-specific/OathKeeper/OathTakerSearch/OathTakerSearch.js
+++ src/components/app-specific/OathKeeper/OathTakerSearch/OathTakerSearch.js
@@ -3,5 +3,7 @@ import "./OathTakerSearch.scss";
 
 import Form from "JurCommon/Form";
 
-const OathTakerSearch = ({ onChange }) => <Form.Search onChange={onChange} />;
+const OathTakerSearch = ({ onChange }) => (
+  <Form.Search className="jur-oath-taker-search" onChange={onChange} />
+);
 export default OathTakerSearch;
