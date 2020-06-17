23c57ddb9699cd4ec31ec68fcb77c8957a5131a1
Ashish Gaikwad <ashish@jur.io>
2020-06-17 11:59:35 +0530

    chore(dApp): create_react_component can create .stories.js
    Now create react component takes folder path without component name.
    It auto creates a story for common component

--- bin/create_react_component.sh
+++ bin/create_react_component.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-echo "creating react comonent src/components/${1}"
-mkdir -p src/components/${1}
+destination=src/components/${1}/${2}
 
-pushd src/components/${1}
+function storyTemplate () {
+  Component=$1
+  cat << EOM
+import React from "react";
+
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+
+import ${Component} from './';
+
+storiesOf('${Component}', module)
+  .addDecorator(withInfo)
+  .addParameters({
+    info: {
+      inline: true,
+      header: false
+    }
+  })
+  .add('Sample [Auto]', () => (<${Component} />));
+EOM
+}
+
+echo "creating react comonent $destination"
+mkdir -p $destination
+
+pushd $destination
 touch ${2}.scss
 
 echo "import React from 'react';
@@ -13,6 +37,14 @@ export default ${2};" >> ${2}.js
 
 echo "import ${2} from './${2}';
 export default ${2};" >> index.js
+
+if [ $1 == "common" ]
+  then
+    while read line
+      do
+        echo $line >> ${2}.stories.js
+      done < <(storyTemplate $2)
+fi
 
 echo "created $(ls)"
 popd
