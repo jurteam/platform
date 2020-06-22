252bc08d8a64d29dd7dea95fec11d26b9c3a5c71
MTM Suhail <getmtm@gmail.com>
2020-06-22 10:59:14 +0530

    chore(PollingService): created migrations for transaction_states

---
+++ polling-service/producer/database/migrations/2020_06_19_103750_create_transaction_states_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_states', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('instance_id');
+            $table->enum('service_name',['RTR', 'PER']);
+            $table->unsignedBigInteger('last_read_block');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_states');
+    }
+}
