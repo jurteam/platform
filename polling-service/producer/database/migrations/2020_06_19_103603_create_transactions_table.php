447e328fafefafa0d50ce267a752e6b3d8c4d2a1
MTM Suhail <getmtm@gmail.com>
2020-06-22 10:57:55 +0530

    chore(PollingService): created migrations for transactions

---
+++ polling-service/producer/database/migrations/2020_06_19_103603_create_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('asset_id');
+            $table->string('transaction_hash');
+            $table->string('sender');
+            $table->dateTime('timestamp');
+            $table->json('data');
+            $table->timestamps();
+
+            $table->foreign('asset_id')->references('id')->on('assets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
