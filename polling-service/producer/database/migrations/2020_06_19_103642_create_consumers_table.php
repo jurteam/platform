dfa531906329a97447ec4af1fc0ff0dde9b6edb6
MTM Suhail <getmtm@gmail.com>
2020-06-22 10:58:05 +0530

    chore(PollingService): created migrations for consumers

---
+++ polling-service/producer/database/migrations/2020_06_19_103642_create_consumers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consumers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('asset_id');
+            $table->string('consumer_name');
+            $table->string('url');
+            $table->timestamps();
+
+            $table->foreign('asset_id')->references('id')->on('assets')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consumers');
+    }
+}
