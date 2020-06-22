294631ec922b6d621eef318acacac7a0210fef10
Ashish Gaikwad <ashish@jur.io>
2020-06-22 12:19:27 +0530

    chore(Status): adds polling service dockers

--- bin/build_dockers.sh
+++ bin/build_dockers.sh
@@ -1,1 +1,2 @@
 docker build -t jur_platform:08JUN2020 -f Dockerfile .
+docker build -t polling_producer:22JUN2020 -f polling-service/producer/Dockerfile polling-service/producer
--- bin/provision_dockers.sh
+++ bin/provision_dockers.sh
@@ -1,1 +1,2 @@
 sudo chown -R www-data:www-data rest/storage/logs
+sudo chown -R www-data:www-data polling-service/producer/storage/logs
