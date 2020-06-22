1f7e1d5cfc12f99280c20dea2f90b7c4639c4a77
MTM Suhail <getmtm@gmail.com>
2020-06-22 10:57:44 +0530

    chore(PollingService): created migrations for assets

---
+++ polling-service/producer/database/migrations/2020_06_19_103509_create_assets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('asset_name');
+            $table->string('contract_address');
+            $table->string('event_name');
+            $table->json('event_abi');
+            $table->unsignedBigInteger('default_from_block');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assets');
+    }
+}
