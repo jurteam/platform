28e31ac1c64f716e15185c640ffb7c177dae208c
Shuchi Tyagi <shuchityagi@Shuchis-MacBook-Pro.local>
2020-06-26 19:51:19 +0530

    feat(PER): updated end-points

--- polling-service/node-services/src/per-api.js
+++ polling-service/node-services/src/per-api.js
@@ -5,42 +5,36 @@ const server = fastify({ logger: true });
 const PORT = process.env.PER_PORT || 3000;
 
 const createServer = () => {
-  server.post(
-    "/polling-service/past-events/blocks/:blockNumber",
-    (request, response) => {
-      const { instanceId, contracts } = request.body;
-      const parser = new Parser(contracts);
-      blockchain
-        .block(request.params.blockNumber)
-        .then(blockchain.transactions)
-        .then(receipts => parser.parse(receipts))
-        .then(txData => {
-          return { data: txData, instanceId: instanceId };
-        })
-        .then(data => response.code(200).send(data))
-        .catch(e => {
-          response.code(400).send(e);
-        });
-    }
-  );
+  server.post("/blocks/:blockNumber", (request, response) => {
+    const { instanceId, contracts } = request.body;
+    const parser = new Parser(contracts);
+    blockchain
+      .block(request.params.blockNumber)
+      .then(blockchain.transactions)
+      .then(receipts => parser.parse(receipts))
+      .then(txData => {
+        return { data: txData };
+      })
+      .then(data => response.code(200).send(data))
+      .catch(e => {
+        response.code(400).send(e);
+      });
+  });
 
-  server.post(
-    "/polling-service/past-events/tx/:transactionHash",
-    (request, response) => {
-      const { contracts } = request.body;
-      const parser = new Parser(contracts);
-      blockchain
-        .receipt(request.params.transactionHash)
-        .then(receipts => parser.parseTx(receipts))
-        .then(txData => {
-          return { data: txData };
-        })
-        .then(data => response.code(200).send(data))
-        .catch(e => {
-          response.code(400).send(e);
-        });
-    }
-  );
+  server.post("/tx/:transactionHash", (request, response) => {
+    const { contracts } = request.body;
+    const parser = new Parser(contracts);
+    blockchain
+      .receipt(request.params.transactionHash)
+      .then(receipts => parser.parseTx(receipts))
+      .then(txData => {
+        return { data: txData };
+      })
+      .then(data => response.code(200).send(data))
+      .catch(e => {
+        response.code(400).send(e);
+      });
+  });
 
   server.listen(PORT, err => {
     if (err) {
