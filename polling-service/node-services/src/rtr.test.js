7dda19457335103011c516bb3a2353425cad6ee4
Ashish Gaikwad <ashish@jur.io>
2020-06-26 15:00:41 +0530

    chore(RTR): removes unused files

---
+++ polling-service/node-services/src/rtr.test.js
@@ -0,0 +1,9 @@
+describe("RTR", () => {
+  it("asks for config on start");
+  it("exit if no config is received");
+  it("restarts if exited programatically");
+  it("exposes API to read current config");
+  it("connects to blockchain");
+  it("exit if failed to connect to blockchain");
+  it("sends data to producer if interested events found");
+});
