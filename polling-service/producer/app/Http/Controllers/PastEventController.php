35c81e756aa9e568892da347d618a804c3f36a79
MTM Suhail <getmtm@gmail.com>
2020-06-26 16:42:39 +0530

    chore(PollingService): fetch PER host from config

--- polling-service/producer/app/Http/Controllers/PastEventController.php
+++ polling-service/producer/app/Http/Controllers/PastEventController.php
@@ -5,4 +5,19 @@ use Dingo\Api\Routing\Helpers;
 class PastEventController extends Controller
 {
     use Helpers;
+
+    /**
+     * Host of PER service.
+     */
+    private $host;
+
+    /**
+     * Instantiate a new PastEventController instance.
+     */
+    public function __construct()
+    {
+        // get host info from config
+        $this->host = config('polling.PERHost');
+    }
+
 }
