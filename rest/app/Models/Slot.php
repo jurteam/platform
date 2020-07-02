e639989edb18c338da0b6dff295ee01193d4e37f
MTM Suhail <getmtm@gmail.com>
2020-07-02 16:06:54 +0530

    chore(Reward): added function `SlotAssigned`

--- rest/app/Models/Slot.php
+++ rest/app/Models/Slot.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Slot extends Model
 {
-    //
+    /**
+     * Create Slot when `SlotAssigned` event triggered
+     *
+     * @param Object $payload: payload  send by Smart-Contract event
+     * @return Boolean the success or failure message
+     */
+    public static function slotAssigned($payload)
+    {
+        // find reward_activity
+        $rewardActivity = RewardActivity::where('sc_activity_id', $payload->activityId)->firstOrFail();
+
+        // Update RewardActivity
+        $slot = Slot::firstOrCreate(['sc_slot_id' => $payload->slotId, 'reward_activity_id' => $rewardActivity->id])->firstOrFail();
+        $slot->assigned_wallet = $payload->slotCount;
+        $slot->due_date = Carbon::createFromTimestamp($payload->dueDate);
+        $slot->status = 'Assigned';
+        $slot->created_at = Carbon::createFromTimestamp($payload->createdAt);
+
+        return $slot->save();
+    }
 }
