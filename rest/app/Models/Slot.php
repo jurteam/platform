51bfc3d4ad411c0437b49d2e6af4996d0a59bbda
MTM Suhail <getmtm@gmail.com>
2020-07-02 16:38:40 +0530

    chore(Reward): added function `slotUpdated`

--- rest/app/Models/Slot.php
+++ rest/app/Models/Slot.php
@@ -27,4 +27,22 @@ class Slot extends Model
 
         return $slot->save();
     }
+
+    /**
+     * Update Slot when `SlotUpdated` event triggered
+     *
+     * @param Object $payload: payload  send by Smart-Contract event
+     * @return Boolean the success or failure message
+     */
+    public static function slotUpdated($payload)
+    {
+        // find reward_activity
+        $rewardActivity = RewardActivity::where('sc_activity_id', $payload->activityId)->firstOrFail();
+
+        // Update RewardActivity
+        $slot = Slot::firstOrCreate(['sc_slot_id' => $payload->slotId, 'reward_activity_id' => $rewardActivity->id]);
+        $slot->status = $payload->newState;
+
+        return $slot->save();
+    }
 }
