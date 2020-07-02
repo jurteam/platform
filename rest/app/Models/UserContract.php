7afd2024be18525ebbc3645076896430fa02837f
MTM Suhail <getmtm@gmail.com>
2020-07-02 13:37:11 +0530

    chore(Reward): added function  `UserContractUpdated`

--- rest/app/Models/UserContract.php
+++ rest/app/Models/UserContract.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserContract extends Model
 {
-    //
+    /**
+     * Store/Update user-contract when `UserContractUpdated` event triggered
+     *
+     * @param Object $payload: payload  send by Smart-Contract event
+     * @return Boolean the success or failure message
+     */
+    public static function UserContractUpdated($payload)
+    {
+        $userContract = UserContract::firstOrCreate(['sc_user_type_id' => $payload->id]);
+        $userContract->sc_user_type_id = $payload->id;
+        $userContract->contract_address = $payload->address;
+        $userContract->name = $payload->name;
+        $userContract->is_active = $payload->state;
+
+        return $userContract->save();
+    }
 }
