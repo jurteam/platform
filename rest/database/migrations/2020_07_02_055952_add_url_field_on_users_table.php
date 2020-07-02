b3a0aa65cbe53977686528274d6cbafebbc34fcc
MTM Suhail <getmtm@gmail.com>
2020-07-02 11:37:16 +0530

    feat(Reward): created migrations for adding  `url` field to `User` table

---
+++ rest/database/migrations/2020_07_02_055952_add_url_field_on_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddUrlFieldOnUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('url')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('url');
+        });
+    }
+}
