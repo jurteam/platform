ab125506fd1bcc7d359932fca99c55a7bc19c144
MTM Suhail <getmtm@gmail.com>
2020-07-02 11:00:41 +0530

    fix(Status): typo

--- rest/database/migrations/2020_06_17_062305_create_statuses_table.php
+++ rest/database/migrations/2020_06_17_062305_create_statuses_table.php
@@ -22,7 +22,7 @@ class CreateStatusesTable extends Migration
             $table->enum('status_type', ['Solomon', 'Justinian']);
             $table->timestamps();
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');;
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
