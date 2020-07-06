53cc21171ab6134e4f285ebdb119c7458c981889
MTM Suhail <getmtm@gmail.com>
2020-07-06 18:50:30 +0530

    feat(Reward): added consumer seed for Rewards

--- polling-service/producer/database/seeds/LocalConsumerSeeder.php
+++ polling-service/producer/database/seeds/LocalConsumerSeeder.php
@@ -35,6 +35,54 @@ class LocalConsumerSeeder extends Seeder
                 'consumer_name' => 'Advocate',
                 'url' => 'http://172.18.0.8/api/v1/polling/advocate'
             ]);
+
+            DB::table('consumers')->insert([
+                'contract_address' => '0x3C09b97c50fcb55DFD5d610f763c61246E03F478',
+                'asset_name' => 'jurRewards',
+                'event_name' => 'RoleContractUpdated',
+                'consumer_name' => 'Reward',
+                'url' => 'http://172.18.0.8/api/v1/polling/reward'
+            ]);
+
+            DB::table('consumers')->insert([
+                'contract_address' => '0x3C09b97c50fcb55DFD5d610f763c61246E03F478',
+                'asset_name' => 'jurRewards',
+                'event_name' => 'ActivityCreated',
+                'consumer_name' => 'Reward',
+                'url' => 'http://172.18.0.8/api/v1/polling/reward'
+            ]);
+
+            DB::table('consumers')->insert([
+                'contract_address' => '0x3C09b97c50fcb55DFD5d610f763c61246E03F478',
+                'asset_name' => 'jurRewards',
+                'event_name' => 'SlotAssigned',
+                'consumer_name' => 'Reward',
+                'url' => 'http://172.18.0.8/api/v1/polling/reward'
+            ]);
+
+            DB::table('consumers')->insert([
+                'contract_address' => '0x3C09b97c50fcb55DFD5d610f763c61246E03F478',
+                'asset_name' => 'jurRewards',
+                'event_name' => 'ActivityUpdated',
+                'consumer_name' => 'Reward',
+                'url' => 'http://172.18.0.8/api/v1/polling/reward'
+            ]);
+
+            DB::table('consumers')->insert([
+                'contract_address' => '0x3C09b97c50fcb55DFD5d610f763c61246E03F478',
+                'asset_name' => 'jurRewards',
+                'event_name' => 'SlotUpdated',
+                'consumer_name' => 'Reward',
+                'url' => 'http://172.18.0.8/api/v1/polling/reward'
+            ]);
+
+            DB::table('consumers')->insert([
+                'contract_address' => '0x3C09b97c50fcb55DFD5d610f763c61246E03F478',
+                'asset_name' => 'jurRewards',
+                'event_name' => 'SlotRewarded',
+                'consumer_name' => 'Reward',
+                'url' => 'http://172.18.0.8/api/v1/polling/reward'
+            ]);
         }
     }
 }
