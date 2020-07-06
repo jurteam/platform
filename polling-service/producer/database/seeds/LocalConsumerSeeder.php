5c96f6ffbfd04ce087f1d80e165cbd75f09504a1
MTM Suhail <getmtm@gmail.com>
2020-07-06 15:10:09 +0530

    fix(Reward): typo

--- polling-service/producer/database/seeds/LocalConsumerSeeder.php
+++ polling-service/producer/database/seeds/LocalConsumerSeeder.php
@@ -13,24 +13,24 @@ class LocalConsumerSeeder extends Seeder
     {
         {
             DB::table('consumers')->insert([
-                'contract_address' => 'jurAdvocate',
-                'asset_name' => '0x79481d8933832e63be499beaddcd49ac9a809fef',
+                'contract_address' => '0x79481d8933832e63be499beaddcd49ac9a809fef',
+                'asset_name' => 'jurAdvocate',
                 'event_name' => 'AdvocateAdded',
                 'consumer_name' => 'Advocate',
                 'url' => 'http://172.18.0.8/api/v1/polling/advocate'
             ]);
 
             DB::table('consumers')->insert([
-                'contract_address' => 'jurAdvocate',
-                'asset_name' => '0x79481d8933832e63be499beaddcd49ac9a809fef',
+                'contract_address' => '0x79481d8933832e63be499beaddcd49ac9a809fef',
+                'asset_name' => 'jurAdvocate',
                 'event_name' => 'AdvocateStateUpdated',
                 'consumer_name' => 'Advocate',
                 'url' => 'http://172.18.0.8/api/v1/polling/advocate'
             ]);
 
             DB::table('consumers')->insert([
-                'contract_address' => 'jurAdvocate',
-                'asset_name' => '0x79481d8933832e63be499beaddcd49ac9a809fef',
+                'contract_address' => '0x79481d8933832e63be499beaddcd49ac9a809fef',
+                'asset_name' => 'jurAdvocate',
                 'event_name' => 'AdvocateTypeUpdated',
                 'consumer_name' => 'Advocate',
                 'url' => 'http://172.18.0.8/api/v1/polling/advocate'
