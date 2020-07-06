d47d2f758d7cc815caf07bde8c03b53d4cb60b0a
MTM Suhail <getmtm@gmail.com>
2020-07-06 08:42:37 +0530

    chore(Reward): created function to handle `advocateAdded` event

--- rest/app/Models/Advocate.php
+++ rest/app/Models/Advocate.php
@@ -6,5 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Advocate extends Model
 {
-    //
+    /**
+     * Store an advocate when `AdvocateAdded` event triggered
+     *
+     * @param Object $payload: payload  send by Smart-Contract event
+     * @return Boolean the success or failure message
+     */
+    public static function advocateAdded($payload)
+    {
+        // Check record exisits
+        $exists = Advocate::where('wallet', $payload->wallet)->first();
+
+        // ignore creation if already exists
+        if (isset($exists)) {
+            Log::warning('The Advocate with wallet `' . $payload->wallet . '` already exists in the database.');
+            return false;
+        }
+
+        // Save new advocate
+        $advocate = new Advocate;
+        $advocate->wallet = $payload->wallet;
+        $advocate->activation_time = $payload->activation_time;
+        $advocate->is_active = true;
+        $advocate->type = $payload->advocateType;
+
+        return $advocate->save();
+    }
+
 }
