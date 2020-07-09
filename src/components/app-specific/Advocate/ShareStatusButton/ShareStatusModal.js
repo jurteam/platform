009f06d2f5b75930f41bea979c9dafb75603c30b
Ashish Gaikwad <ashish@jur.io>
2020-07-09 14:53:46 +0530

    copy(Advocate): updates title of share modal
    Resolves: #256

--- src/components/app-specific/Advocate/ShareStatusButton/ShareStatusModal.js
+++ src/components/app-specific/Advocate/ShareStatusButton/ShareStatusModal.js
@@ -8,13 +8,14 @@ import SocialNetworkInput from "../SocialNetworkInput";
 import ShareStatusTextInput from "../ShareStatusTextInput";
 import SubmitShareButton from "../SubmitShareButton";
 import { getAdvocateMessage } from "../../../../sagas/Selectors";
+import { isMyProfile } from "../../../../utils/AdvocateHelpers";
 
-const ShareModal = ({ address, isOpen, onRequestClose, message }) => {
+const ShareModal = ({ address, title, isOpen, onRequestClose, message }) => {
   const initialText = `I am an Advocate in the Jur ecosystem to support the development of a truly decentralized ecosystem for a new legal framework https://status.jur.io/${address} `;
 
   return (
     <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <Modal.Header title="Share your Status Badge" />
+      <Modal.Header title={modalTitle(address)} />
       <Modal.Body>
         <Text weight="bold" size="small">
           Select a network
@@ -31,6 +32,12 @@ const ShareModal = ({ address, isOpen, onRequestClose, message }) => {
     </Modal>
   );
 };
+
+function modalTitle(address) {
+  return isMyProfile(address)
+    ? "Share your Advocate Badge"
+    : "Share Advocate Badge";
+}
 
 const mapStateToProps = state => ({
   message: getAdvocateMessage(state)
