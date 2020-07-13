272fe9660705bc95ac0364fe1385e68e3a14a463
Ashish Gaikwad <ashish@jur.io>
2020-07-13 15:58:14 +0530

    feat(Advocate): adds search input on listing page

--- src/components/app-specific/Advocate/AdvocatesHeader/AdvocatesHeader.js
+++ src/components/app-specific/Advocate/AdvocatesHeader/AdvocatesHeader.js
@@ -2,16 +2,24 @@ import React from "react";
 import "./AdvocatesHeader.scss";
 
 import Text from "JurCommon/Text";
+import Row from "JurCommon/Row";
+import Col from "JurCommon/Col";
+import AdvocatesSearchInput from "../AdvocatesSearchInput";
 
 const AdvocatesHeader = () => (
-  <>
-    <Text size="large" transform="header">
-      Advocates
-    </Text>
-    <Text>
-      This is the list of active Jur Advocates licenses that are co-creating the
-      Jur legal automation framework
-    </Text>
-  </>
+  <Row>
+    <Col>
+      <Text size="large" transform="header">
+        Advocates
+      </Text>
+      <Text>
+        This is the list of active Jur Advocates licenses that are co-creating
+        the Jur legal automation framework
+      </Text>
+    </Col>
+    <Col>
+      <AdvocatesSearchInput />
+    </Col>
+  </Row>
 );
 export default AdvocatesHeader;
