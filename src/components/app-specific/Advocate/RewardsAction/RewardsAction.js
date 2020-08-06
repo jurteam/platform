7fa91249ea47f28c15135961ed3ba650b824cfb1
Ashish Gaikwad <ashish@jur.io>
2020-08-06 12:14:11 +0530

    feature(Advocate): shows timer in Rewards table
    Also on time out shows Withdraw button

--- src/components/app-specific/Advocate/RewardsAction/RewardsAction.js
+++ src/components/app-specific/Advocate/RewardsAction/RewardsAction.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RewardsAction.scss";
 import { i18nDateFormatSec } from "../../../../utils/helpers";
-import { canWithdraw, isMyProfile } from "JurUtils/AdvocateHelpers";
+import {
+  canWithdraw,
+  isMyProfile,
+  formatWaiting,
+  withdrawTimeOf
+} from "JurUtils/AdvocateHelpers";
+import Timer from "JurCommon/Timer";
 import WithdrawButton from "./WithdrawButton";
 import { getWallet, getLabels } from "../../../../sagas/Selectors";
 
-const RewardsAction = ({ activity, isPublic, labels }) =>
-  !isPublic && canWithdraw(activity) ? (
-    <WithdrawButton
-      activityScId={activity.activityScId}
-      slotScId={activity.slotScId}
-      rewardAmount={activity.rewardAmount}
-    />
-  ) : (
-    <span>
-      {activity.rewardedOn
-        ? i18nDateFormatSec(activity.rewardedOn)
-        : labels.notCreditedYet}
-    </span>
-  );
+const RewardsAction = ({ activity, isPublic, labels }) => {
+  const [display, setDisplay] = useState(computeDisplay(activity, isPublic));
+
+  switch (display) {
+    case "withdraw":
+      return (
+        <WithdrawButton
+          activityScId={activity.activityScId}
+          slotScId={activity.slotScId}
+          rewardAmount={activity.rewardAmount}
+        />
+      );
+    case "rewarded":
+      return <span>{i18nDateFormatSec(activity.rewardedOn)}</span>;
+    case "waiting":
+      return (
+        <Timer
+          time={withdrawTimeOf(activity.dueDate)}
+          render={formatWaiting}
+          onPast={() => setDisplay(computeDisplay(activity, isPublic))}
+        />
+      );
+  }
+};
+
+const computeDisplay = (activity, isPublic) => {
+  if (!isPublic && canWithdraw(activity)) return "withdraw";
+  if (activity.rewardedOn) return "rewarded";
+  return "waiting";
+};
 
 const mapStateToProps = state => {
   const { address } = getWallet(state);
