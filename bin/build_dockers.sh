9e97de0892f63738bc84d590db6b6b308134b464
MTM Suhail <getmtm@gmail.com>
2020-08-12 17:47:46 +0530

    fix(Docker): Reference #378

--- bin/build_dockers.sh
+++ bin/build_dockers.sh
@@ -1,2 +1,2 @@
-docker build -t jur_platform:12AUG2020 -f Dockerfile .
-docker build -t polling_producer:15JUL2020 -f polling-service/Dockerfile ./polling-service
+docker build -t jur_platform:13AUG2020 -f Dockerfile .
+docker build -t polling_producer:13AUG2020 -f polling-service/Dockerfile ./polling-service
